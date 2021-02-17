a72412b1cce288a8e6f7d04e1f2382dea112f670
Philippe Voinov <philippevoinov@gmail.com>
2021-02-17 18:14:44 +0100

    Add validation to remaining API calls

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,10 +3,15 @@ import { AccountService } from './AccountService';
 import {
   AgeDistributionEntrySchema,
   Country,
+  CountrySchema,
+  GrowingVariant,
+  GrowingVariantSchema,
   InternationalTimeDistributionEntrySchema,
   SampleResultList,
   SampleResultListSchema,
   TimeDistributionEntrySchema,
+  Variant,
+  VariantSchema,
 } from './api-types';
 
 export enum DistributionType {
@@ -115,24 +120,37 @@ export const getSamples = (
     .then(data => SampleResultListSchema.parse(data));
 };
 
-export const getGrowingVariants = (year: number, week: number, country: string, signal?: AbortSignal) => {
+export const getGrowingVariants = (
+  year: number,
+  week: number,
+  country: string,
+  signal?: AbortSignal
+): Promise<GrowingVariant[]> => {
   const endpoint = `/computed/find-growing-variants?year=${year}&week=${week}&country=${country}`;
   const url = HOST + endpoint;
-  return fetch(url, { headers: getBaseHeaders(), signal }).then(response => response.json());
+  return fetch(url, { headers: getBaseHeaders(), signal })
+    .then(response => response.json())
+    .then(data => zod.array(GrowingVariantSchema).parse(data));
 };
 
-export const getVariants = () => {
+export const getVariants = (): Promise<Variant[]> => {
   const url = HOST + '/resource/variant';
-  return fetch(url, { headers: getBaseHeaders() }).then(response => response.json());
+  return fetch(url, { headers: getBaseHeaders() })
+    .then(response => response.json())
+    .then(data => zod.array(VariantSchema).parse(data));
 };
 
-export const getCurrentWeek = () => {
+export const getCurrentWeek = (): Promise<number> => {
   const url = HOST + '/utils/current-week';
-  return fetch(url, { headers: getBaseHeaders() }).then(response => response.json());
+  return fetch(url, { headers: getBaseHeaders() })
+    .then(response => response.json())
+    .then(data => zod.number().parse(data));
 };
 
-export const getCountries = () => {
+export const getCountries = (): Promise<Country[]> => {
   const url = HOST + '/resource/country';
-  return fetch(url, { headers: getBaseHeaders() }).then(response => response.json());
+  return fetch(url, { headers: getBaseHeaders() })
+    .then(response => response.json())
+    .then(data => zod.array(CountrySchema).parse(data));
 };
 export const fetchTimeDistributionData = () => {};
