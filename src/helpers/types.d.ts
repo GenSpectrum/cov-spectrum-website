48260a6d0a08a86a03fa689f0b37fa3ec69eabf7
Philippe Voinov <philippevoinov@gmail.com>
2021-02-17 18:22:56 +0100

    Remove old unused types

--- src/helpers/types.d.ts
+++ src/helpers/types.d.ts
@@ -4,20 +4,3 @@ export interface DataDistributionConfiguration {
   mutations: string[];
   variant?: string;
 }
-
-export type Week = {
-  firstDayInWeek: string;
-  yearWeek: string;
-};
-
-// export type DistributionData = DistributionDataPoint[] | undefined;
-
-type DistributionY = {
-  count: number;
-  proportion: {
-    ciLower: number;
-    ciUpper: number;
-    confidenceLevel: number;
-    value: number;
-  };
-};
