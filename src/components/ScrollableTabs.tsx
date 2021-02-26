439a71cf61d14bf12c09b29e7b2b0e478488f04d
Philippe Voinov <philippevoinov@gmail.com>
2021-02-26 18:43:43 +0100

    Extract ScrollableTabs into component

---
+++ src/components/ScrollableTabs.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Nav, Tab } from 'react-bootstrap';
+import styled from 'styled-components';
+import { scrollableContainerStyle } from '../helpers/scrollable-container';
+
+const TabsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const TabContentWrapper = styled.div`
+  height: 100px;
+  flex-grow: 1;
+`;
+
+const ScrollableTabPane = styled.div`
+  ${scrollableContainerStyle}
+`;
+
+interface Props {
+  tabs: TabDefinition[];
+}
+
+export interface TabDefinition {
+  key: string;
+  title: string;
+  content: React.ReactChild | React.ReactChild[];
+}
+
+export const ScrollableTabs = ({ tabs }: Props) => {
+  return (
+    <Tab.Container defaultActiveKey='explore' id='variantList' transition={false} unmountOnExit>
+      <TabsWrapper>
+        <Nav variant='tabs'>
+          {tabs.map(tab => (
+            <Nav.Item key={tab.key}>
+              <Nav.Link eventKey={tab.key}>{tab.title}</Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+        <Tab.Content as={TabContentWrapper}>
+          {tabs.map(tab => (
+            <Tab.Pane as={ScrollableTabPane} key={tab.key} eventKey={tab.key}>
+              {tab.content}
+            </Tab.Pane>
+          ))}
+        </Tab.Content>
+      </TabsWrapper>
+    </Tab.Container>
+  );
+};
--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { Nav, Tab } from 'react-bootstrap';
 import { KnownVariantsList } from '../components/KnownVariantsList';
 import { MutationLookup } from '../components/MutationLookup';
+import { NamedSection } from '../components/NamedSection';
 import { NewVariantLookup } from '../components/NewVariantLookup';
 import { Country, Variant } from '../services/api-types';
-import { NamedSection } from '../components/NamedSection';
-import { scrollableContainerStyle } from '../helpers/scrollable-container';
-import styled from 'styled-components';
-
-const TabsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
-
-const TabContentWrapper = styled.div`
-  height: 100px;
-  flex-grow: 1;
-`;
-
-const ScrollableTabPane = styled.div`
-  ${scrollableContainerStyle}
-`;
+import { ScrollableTabs } from '../components/ScrollableTabs';
 
 interface Props {
   country: Country;
@@ -30,33 +13,32 @@ interface Props {
 
 export const ExplorePage = ({ country, onVariantSelect }: Props) => {
   return (
-    <Tab.Container defaultActiveKey='explore' id='variantList' transition={false} unmountOnExit>
-      <TabsWrapper>
-        <Nav variant='tabs'>
-          <Nav.Item>
-            <Nav.Link eventKey='explore'>Explore</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Tab.Content as={TabContentWrapper}>
-          <Tab.Pane as={ScrollableTabPane} eventKey='explore'>
-            <NamedSection title='Known variants'>
-              <KnownVariantsList
-                country={country}
-                onVariantSelect={variant => onVariantSelect({ variant, matchPercentage: 0.8 })}
-              />
-            </NamedSection>
-            <NamedSection title='Potential new variants'>
-              <NewVariantLookup
-                country={country}
-                onVariantSelect={variant => onVariantSelect({ variant, matchPercentage: 1 })}
-              />
-            </NamedSection>
-            <NamedSection title='Search by mutations'>
-              <MutationLookup onVariantSelect={onVariantSelect} />
-            </NamedSection>
-          </Tab.Pane>
-        </Tab.Content>
-      </TabsWrapper>
-    </Tab.Container>
+    <ScrollableTabs
+      tabs={[
+        {
+          key: 'explore',
+          title: 'Explore',
+          content: (
+            <>
+              <NamedSection title='Known variants'>
+                <KnownVariantsList
+                  country={country}
+                  onVariantSelect={variant => onVariantSelect({ variant, matchPercentage: 0.8 })}
+                />
+              </NamedSection>
+              <NamedSection title='Potential new variants'>
+                <NewVariantLookup
+                  country={country}
+                  onVariantSelect={variant => onVariantSelect({ variant, matchPercentage: 1 })}
+                />
+              </NamedSection>
+              <NamedSection title='Search by mutations'>
+                <MutationLookup onVariantSelect={onVariantSelect} />
+              </NamedSection>
+            </>
+          ),
+        },
+      ]}
+    />
   );
 };
