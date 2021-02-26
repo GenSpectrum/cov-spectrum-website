24b9f77579c3042ae2d8a8c4d8356cf29b1eba51
Philippe Voinov <philippevoinov@gmail.com>
2021-02-26 15:27:27 +0100

    Move global CSS imports to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 ReactDOM.render(
   <React.StrictMode>
