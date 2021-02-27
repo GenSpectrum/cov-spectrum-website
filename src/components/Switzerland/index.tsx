656b143f37172009f07d3348ea9956e09bc7bf9d
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-02-27 12:44:36 +0100

    scale works

--- src/components/Switzerland/index.tsx
+++ src/components/Switzerland/index.tsx
@@ -9,6 +9,8 @@ import * as zod from 'zod';
 import { SampleSelectorSchema } from '../../helpers/sample-selector';
 import { scaleQuantile, ScaleQuantile } from 'd3-scale';
 
+import styled from 'styled-components';
+
 import bbox from '@turf/bbox';
 import relief from './relief.jpg';
 import geoJson from './PLZ10.json';
@@ -16,11 +18,13 @@ import geoJson from './PLZ10.json';
 const PropsSchema = SampleSelectorSchema;
 type Props = zod.infer<typeof PropsSchema> & { width: number };
 
+const Wrapper = styled.div`
+  padding: 1rem 1rem 1rem 1rem;
+`;
+
 const Switzerland = ({ country, mutations, matchPercentage, width = 1000 }: Props) => {
   const [minX, minY, maxX, maxY] = bbox(geoJson);
-  const [distributionData, setDistributionData] = useState<TimeZipCodeDistributionEntry[] | undefined>(
-    undefined
-  );
+  const [distributionData, setDistributionData] = useState<TimeZipCodeDistributionEntry[]>([]);
 
   const loggedIn = AccountService.isLoggedIn();
 
@@ -42,26 +46,41 @@ const Switzerland = ({ country, mutations, matchPercentage, width = 1000 }: Prop
 
   // svg paths from geoJson feature
   const path = geoPath().projection(projection);
-  const [colorScale, setColorScale] = useState<undefined | ScaleQuantile<string, number>>(undefined);
+  // const [colorScale, setColorScale] = useState<undefined | ScaleQuantile<string>>(undefined);
 
-  useEffect(() => {
-    if (distributionData !== undefined) {
-      const newScale = scaleQuantile<string, number>()
-        .domain(distributionData.map((d: TimeZipCodeDistributionEntry) => d.y.count))
-        .range([
-          '#ffedea',
-          '#ffcec5',
-          '#ffad9f',
-          '#ff8a75',
-          '#ff5533',
-          '#e2492d',
-          '#be3d26',
-          '#9a311f',
-          '#782618',
-        ]);
-      setColorScale(newScale);
-    }
-  }, [distributionData]);
+  // useEffect(() => {
+  //   if (distributionData !== undefined) {
+  //     const newScale = scaleQuantile<string>()
+  //       .domain(distributionData.map((d: TimeZipCodeDistributionEntry) => d.y.count))
+  //       .range([
+  //         '#ffedea',
+  //         '#ffcec5',
+  //         '#ffad9f',
+  //         '#ff8a75',
+  //         '#ff5533',
+  //         '#e2492d',
+  //         '#be3d26',
+  //         '#9a311f',
+  //         '#782618',
+  //       ]);
+  //     setColorScale(newScale);
+  //     console.log('updated color scale to ', newScale);
+  //   }
+  // }, [distributionData]);
+
+  const colorScale = scaleQuantile<string>()
+    .domain(distributionData.map((d: TimeZipCodeDistributionEntry) => d.y.count))
+    .range([
+      '#ffedea',
+      '#ffcec5',
+      '#ffad9f',
+      '#ff8a75',
+      '#ff5533',
+      '#e2492d',
+      '#be3d26',
+      '#9a311f',
+      '#782618',
+    ]);
 
   useEffect(() => {
     let isSubscribed = true;
@@ -72,10 +91,10 @@ const Switzerland = ({ country, mutations, matchPercentage, width = 1000 }: Prop
         if (isSubscribed) {
           setDistributionData(newDistributionData);
         }
-        console.log('new distribution data is', newDistributionData);
+        // console.log('new distribution data is', newDistributionData);
       })
       .catch(e => {
-        console.log('Called fetch data error', e);
+        // console.log('Called fetch data error', e);
       });
     return () => {
       isSubscribed = false;
@@ -84,8 +103,8 @@ const Switzerland = ({ country, mutations, matchPercentage, width = 1000 }: Prop
   }, [country, mutations, matchPercentage]);
 
   return loggedIn && distributionData !== undefined ? (
-    <div>
-      <h1>Number of cases by postal code</h1>
+    <Wrapper>
+      <h2>Number of cases by postal code in Switzerland</h2>
       <div style={{ position: 'relative', width, height }}>
         <img src={relief} style={{ opacity: 0.4, width: '100%', height: 'auto' }} alt='' />
         <svg width={width} height={height} style={{ position: 'absolute', top: 0, left: 0 }}>
@@ -94,21 +113,27 @@ const Switzerland = ({ country, mutations, matchPercentage, width = 1000 }: Prop
             const cur = distributionData.find(
               (s: TimeZipCodeDistributionEntry) => s.x.zipCode == plz.toString()
             );
+            let color: string = '#ffffff00';
+            let caseCount: number = 0;
+            if (cur !== undefined) {
+              caseCount = cur.y.count;
+            }
             return (
               <path
-                data-tip={`${cur != undefined ? cur.y.count : 0} (PLZ ${plz})`}
+                data-tip={`${caseCount} (PLZ ${plz})`}
                 key={`path-${plz}-${feature.properties.UUID}`}
                 stroke='#95a5a6'
                 strokeWidth={0.25}
                 d={path(feature) ?? undefined}
-                fill={cur ? (plz < 5000 ? 'red' : 'blue') : '#ffffff00'}
+                // fill={cur ? (plz < 5000 ? 'red' : 'blue') : '#ffffff00'}
+                fill={cur ? colorScale(caseCount) : '#ffffff00'}
               />
             );
           })}
         </svg>
       </div>
       <ReactTooltip />
-    </div>
+    </Wrapper>
   ) : (
     <div></div>
   );
