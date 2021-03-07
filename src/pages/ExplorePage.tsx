e7d5cd61b92b1fc1d98fe76bc7e694f3365012dd
Chaoran Chen <mail@chaoran-chen.de>
2021-03-08 00:17:38 +0100

    Move definition of known variants to the web app

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -3,11 +3,11 @@ import { KnownVariantsList } from '../components/KnownVariantsList';
 import { MutationLookup } from '../components/MutationLookup';
 import { NamedSection } from '../components/NamedSection';
 import { NewVariantLookup } from '../components/NewVariantLookup';
-import { Country, Variant, Selection } from '../services/api-types';
+import { Country, Selection } from '../services/api-types';
 import { ScrollableTabs } from '../components/ScrollableTabs';
 interface Props {
   country: Country;
-  onVariantSelect: (selection: { variant: Variant; matchPercentage: number }) => void;
+  onVariantSelect: (selection: Selection) => void;
   selection: Selection | undefined;
 }
 
@@ -23,7 +23,7 @@ export const ExplorePage = ({ country, onVariantSelect, selection }: Props) => {
               <NamedSection title='Known variants'>
                 <KnownVariantsList
                   country={country}
-                  onVariantSelect={variant => onVariantSelect({ variant, matchPercentage: 0.8 })}
+                  onVariantSelect={onVariantSelect}
                   selection={selection}
                 />
               </NamedSection>
