0a0f01f4cf2612352fa6fd4c9c7963989e9c6665
Chaoran Chen <mail@chaoran-chen.de>
2021-03-09 12:24:26 +0100

    Fix merge error

--- src/helpers/app-layout.ts
+++ src/helpers/app-layout.ts
@@ -4,7 +4,7 @@ import { scrollableContainerStyle } from './scrollable-container';
 export const OuterWrapper = styled.div`
   display: grid;
   height: 100vh;
-  grid-template-columns: 50% 50%;
+  grid-template-columns: minmax(auto, 700px) auto;
   grid-template-rows: 60px auto;
   grid-template-areas:
     'header header'
