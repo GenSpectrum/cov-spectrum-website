585135e87cae7f516f063fa50d2750b0a138cf7c
Philippe Voinov <philippevoinov@gmail.com>
2021-04-06 12:08:15 +0200

    Add DateCache.weeksFromRange

--- src/helpers/date-cache.ts
+++ src/helpers/date-cache.ts
@@ -18,9 +18,14 @@ export interface UnifiedIsoWeek {
 class DateCache {
   private dayCache = new Map<string, UnifiedDay>();
   private isoWeekCache = new Map<string, UnifiedIsoWeek>();
+  private nextWeekCache = new WeakMap<UnifiedIsoWeek, UnifiedIsoWeek>();
 
   getDay(dayString: string): UnifiedDay {
     return this._getDay(dayString, undefined, undefined);
+  }
+
+  getDayUsingDayjs(dayjsDay: Dayjs): UnifiedDay {
+    return this.getDay(dayjsDay.format('YYYY-MM-DD'));
   }
 
   getIsoWeek(yearWeekString: string): UnifiedIsoWeek {
@@ -63,9 +68,47 @@ class DateCache {
     return min && max && { min, max };
   }
 
+  weeksFromRange(range: { min: UnifiedIsoWeek; max: UnifiedIsoWeek } | undefined): UnifiedIsoWeek[] {
+    if (!range) {
+      return [];
+    }
+
+    const { min, max } = range;
+    assert(this.weekIsBefore(min, max) || min === max);
+
+    const output: UnifiedIsoWeek[] = [min];
+    let last = min;
+    while (last !== max) {
+      const next = this.getNextWeek(last);
+      output.push(next);
+      last = next;
+    }
+    return output;
+  }
+
   // a < b
   private weekIsBefore(a: UnifiedIsoWeek, b: UnifiedIsoWeek) {
     return a.isoYear < b.isoYear || (a.isoYear === b.isoYear && a.isoWeek < b.isoWeek);
+  }
+
+  private getNextWeek(week: UnifiedIsoWeek): UnifiedIsoWeek {
+    let nextWeek = this.nextWeekCache.get(week);
+    if (nextWeek) {
+      return nextWeek;
+    }
+
+    // this is good enough for most weeks of the year
+    nextWeek = this.isoWeekCache.get(`${week.isoYear}-${week.isoWeek + 1}`);
+
+    // cover the case where "week.isoWeek + 1" is out of range at the end of the year
+    if (!nextWeek) {
+      const firstDayOfNextWeek = week.firstDay.dayjs.endOf('isoWeek').add(1, 'day').startOf('isoWeek');
+      nextWeek = this.getDayUsingDayjs(firstDayOfNextWeek).isoWeek;
+    }
+
+    assert(this.weekIsBefore(week, nextWeek));
+    this.nextWeekCache.set(week, nextWeek);
+    return nextWeek;
   }
 
   private _getDay(
@@ -77,6 +120,8 @@ class DateCache {
     if (cachedDay) {
       return cachedDay;
     }
+
+    assert(dayString.match(/^\d{4}-\d{2}-\d{2}$/));
 
     const dayDayjs = _dayDayjs || dayjs(dayString, 'YYYY-MM-DD');
     const isoWeek = _isoWeek || this.getIsoWeek(dayjsToYearWeekString(dayDayjs));
