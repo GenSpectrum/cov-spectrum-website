564793f55b840dda779fb738230fadb23365e844
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-04-30 13:49:13 +0200

    plot data formatting and sorting

--- src/widgets/VariantInternationalComparisonPlot.tsx
+++ src/widgets/VariantInternationalComparisonPlot.tsx
@@ -2,7 +2,7 @@ import { omit, uniqBy } from 'lodash';
 import React, { useMemo } from 'react';
 import * as zod from 'zod';
 import { Plot } from '../components/Plot';
-import { globalDateCache } from '../helpers/date-cache';
+import { globalDateCache, UnifiedIsoWeek } from '../helpers/date-cache';
 import { fillFromWeeklyMap } from '../helpers/fill-missing';
 import { AsyncZodQueryEncoder } from '../helpers/query-encoder';
 import { NewSampleSelectorSchema } from '../helpers/sample-selector';
@@ -17,6 +17,7 @@ import Select from 'react-select';
 
 const digitsForPercent = (v: number): string => (v * 100).toFixed(2);
 
+const CHART_MARGIN_RIGHT = 15;
 interface Props {
   country: Country;
   logScale?: boolean;
@@ -55,8 +56,15 @@ const VariantInternationalComparisonPlot = ({
     console.log("Variant samples by country", variantSamplesByCountry)
     console.log("Whole samples by country", wholeSamplesByCountry)
     // console.log('Variant samples set', variantSampleSet);
-    const mappedVals =  countriesToPlotList.map(
-      ({ name: country, color }): Plotly.Data => {
+    interface ProportionCountry {
+      countryName: string;
+      data: {
+        dateString: string;
+        proportion: number;
+      }[];
+    }
+    const proportionCountries: ProportionCountry[] = countriesToPlotList.map(
+      ({ name: country }) => {
         const variantSampleSet = new SampleSet(variantSamplesByCountry.get(country) ?? [], null);
         const wholeSampleSet = new SampleSet(wholeSamplesByCountry.get(country) ?? [], null);
         const filledData = fillFromWeeklyMap(variantSampleSet.proportionByWeek(wholeSampleSet), {
@@ -68,23 +76,35 @@ const VariantInternationalComparisonPlot = ({
             key,
             value: { ...restValue, proportion: proportion! },
           }));
-          // console.log("international filled data")
+          console.log("international filled data", filledData)
         return {
-          name: country,
-          marker: { color },
-          type: 'scatter',
-          mode: 'lines+markers',
-          x: filledData.map(({ key }) => key.firstDay.string),
-          y: filledData.map(({ value: { proportion } }) => digitsForPercent(proportion)),
-          text: filledData.map(({ value: { proportion } }) => `${digitsForPercent(proportion)}%`),
-          hovertemplate: '%{text}',
+          countryName: country,
+          data: filledData.map((entry) => ({
+            dateString: entry.key.firstDay.string,
+            proportion: entry.value.proportion
+          }))
         };
       }
       );
-      console.log("Mapped values international are...");
-      console.log(mappedVals);
-      return mappedVals;
+      
+      const dateMap: Map<string, any> = new Map();
+
+      for (let {countryName, data} of proportionCountries) {
+          for (let {dateString, proportion} of data) {
+            if (!dateMap.has(dateString)) {
+              dateMap.set(dateString, {
+                dateString,
+              });
+            }
+              dateMap.get(dateString)[countryName] = Math.max(proportion, 0);
+          }
+      }
+  
+      return [...dateMap.values()].sort((a, b) => Date.parse(a.dateString) - Date.parse(b.dateString));
   }, [countriesToPlotList, variantSamplesByCountry, wholeSamplesByCountry, logScale]);
+
+  
+
 
   const xTickVals = useMemo(() => {
     const relevantWeeks = countriesToPlotList.flatMap(({ name }) =>
@@ -102,53 +122,55 @@ const VariantInternationalComparisonPlot = ({
     { value: 'vanilla', label: 'Vanilla' },
   ];
 
+
+  const proportionData = [
+    [
+      {
+        date: '1',
+        proportion: 12,
+      },
+      {
+        date: '2',
+        proportion: 10,
+      },
+    ],
+  ];
+
+
   return (
     <Wrapper>
       <Select
         closeMenuOnSelect={false}
         // defaultValue={[colourOptions[0], colourOptions[1]]}
-        placeholder="Select countries..."
+        placeholder='Select countries...'
         isMulti
         options={options}
         // styles={colourStyles}
       />
       <ChartAndMetricsWrapper>
         <ChartWrapper>
-          <Plot
-            style={{ width: '100%', height: '100%' }}
-            data={plotData}
-            layout={{
-              title: '',
-              xaxis: {
-                title: 'Week',
-                type: 'date',
-                tickvals: xTickVals,
-                tickformat: 'W%-V, %Y',
-                hoverformat: 'Week %-V, %Y (from %d.%m.)',
-              },
-              yaxis: {
-                title: 'Estimated Percentage',
-                type: logScale ? 'log' : 'linear',
-              },
-              legend: {
-                x: 0,
-                xanchor: 'left',
-                y: 1,
-              },
-              margin: {
-                l: 50,
-                r: 40,
-                b: 70,
-                t: 10,
-                pad: 4,
-              },
-            }}
-            config={{
-              displaylogo: false,
-              modeBarButtons: [['zoom2d', 'toImage', 'resetScale2d', 'pan2d']],
-              responsive: true,
-            }}
-          />
+          <ResponsiveContainer>
+            <ComposedChart
+              data={proportionData}
+              margin={{ top: 6, right: CHART_MARGIN_RIGHT, left: 0, bottom: 0 }}
+            >
+              <XAxis
+                dataKey='date'
+                scale='time'
+                type='number'
+              />
+              <YAxis dataKey="proportion" domain={['dataMin', 'auto']} />
+              <Tooltip
+                formatter={(value: any, name: any, props: any) => (value * 100).toFixed(2) + '%'}
+                labelFormatter={label => {
+                  return 'Date: ';
+                }}
+              />
+                <Line dataKey="date" />
+              {/* {plotData.map(location => (
+              ))} */}
+            </ComposedChart>
+          </ResponsiveContainer>
         </ChartWrapper>
         <MetricsWrapper>
           <MetricsSpacing />
@@ -186,3 +208,38 @@ export const VariantInternationalComparisonPlotWidget = new Widget(
   VariantInternationalComparisonPlot,
   'VariantInternationalComparisonPlot'
 );
+
+// const plotData = useMemo(() => {
+//   console.log('Variant samples by country', variantSamplesByCountry);
+//   console.log('Whole samples by country', wholeSamplesByCountry);
+//   // console.log('Variant samples set', variantSampleSet);
+//   const mappedVals = countriesToPlotList.map(
+//     ({ name: country, color }): Plotly.Data => {
+//       const variantSampleSet = new SampleSet(variantSamplesByCountry.get(country) ?? [], null);
+//       const wholeSampleSet = new SampleSet(wholeSamplesByCountry.get(country) ?? [], null);
+//       const filledData = fillFromWeeklyMap(variantSampleSet.proportionByWeek(wholeSampleSet), {
+//         count: 0,
+//         proportion: 0,
+//       })
+//         .filter(({ value: { proportion } }) => proportion !== undefined && (!logScale || proportion > 0))
+//         .map(({ value: { proportion, ...restValue }, key }) => ({
+//           key,
+//           value: { ...restValue, proportion: proportion! },
+//         }));
+//       // console.log("international filled data")
+//       return {
+//         name: country,
+//         marker: { color },
+//         type: 'scatter',
+//         mode: 'lines+markers',
+//         x: filledData.map(({ key }) => key.firstDay.string),
+//         y: filledData.map(({ value: { proportion } }) => digitsForPercent(proportion)),
+//         text: filledData.map(({ value: { proportion } }) => `${digitsForPercent(proportion)}%`),
+//         hovertemplate: '%{text}',
+//       };
+//     }
+//   );
+//   console.log('Mapped values international are...');
+//   console.log(mappedVals);
+//   return mappedVals;
+// }, [countriesToPlotList, variantSamplesByCountry, wholeSamplesByCountry, logScale]);
