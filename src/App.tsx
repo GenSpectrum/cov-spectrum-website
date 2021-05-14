df1017f732910b36ba50e03dc804575ecc962c36
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-05-14 17:36:36 +0200

    two navs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { Header } from './Header';
+import  Header  from './Header';
 import {
   HeaderWrapper,
   LoginWrapper,
--- src/Header.tsx
+++ src/Header.tsx
@@ -91,7 +91,7 @@ const Logo = (
   </div>
 );
 
-export const Header = () => {
+ const Header = () => {
   const loggedIn = AccountService.isLoggedIn();
   let username = null;
   if (loggedIn) {
@@ -100,104 +100,118 @@ export const Header = () => {
 
   return (
     <>
-      {/* <p>Navbar</p> */}
-      <div>
-        <nav className='bg-white dark:bg-gray-800  shadow '>
-          <div className='max-w-9xl mx-auto px-8'>
-            <div className='flex items-center justify-between h-8'>
-              <div className='w-full justify-between flex items-center'>
-                <div id="logo-and-gsid" className="flex flex-row items-center justify-center">
-                  <a className='flex-shrink-0 flex items-center justify-center' href='/'>
-                    {Logo}
-                  </a>
-                  <div className='text-xs flex flex-row'>
-                    <div className='self-end mr-1'>Enabled by data from </div>{' '}
-                    <ExternalLink url='https://gisaid.org/'>
-                      <img src='/img/gisaid.png' alt='GISAID' style={{ height: '20px' }} />{' '}
-                    </ExternalLink>
+      <div className='shadow'>
+        <div id='nav-top'>
+          <nav className='bg-blue '>
+            <div className='max-w-9xl mx-auto px-8'>
+              <div className='flex items-center justify-between h-8'>
+                <div className='w-full justify-between flex items-center'>
+                  <div id='logo-and-gsid' className='flex flex-row items-center justify-center'>
+                    <a className='flex-shrink-0 flex items-center justify-center' href='/'>
+                      {Logo}
+                    </a>
+                    <div className='text-xs flex flex-row'>
+                      <div className='self-end mr-1'>Enabled by data from </div>{' '}
+                      <ExternalLink url='https://gisaid.org/'>
+                        <img src='/img/gisaid.png' alt='GISAID' style={{ height: '20px' }} />{' '}
+                      </ExternalLink>
+                    </div>
                   </div>
-                </div>
-                <div className='hidden md:block items-center justify-center'>
-                  <div className='ml-10 flex items-center space-x-4'>
-                    <a
-                      className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                      href='/acknoledgements'
-                    >
-                      Acknowledgements
-                    </a>
-                    <a
-                      className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                      href='/about'
-                    >
-                      About
-                    </a>
-                    <a
-                      className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
-                      href='/login'
-                    >
-                      Login
-                    </a>
-                    <div
-                      onClick={() =>
-                        window.open('https://github.com/cevo-public/cov-spectrum-website', '_blank')
-                      }
-                    >
-                      <AiOutlineGithub
-                        className='fill-current hover:text-gray-500 rounded-xl filter shadow-xl cursor-pointer'
-                        size={'1.5em'}
-                      />
+                  <div className='hidden md:block items-center justify-center'>
+                    <div className='ml-10 flex items-center space-x-4'>
+                      <a
+                        className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                        href='/acknoledgements'
+                      >
+                        Acknowledgements
+                      </a>
+                      <a
+                        className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                        href='/about'
+                      >
+                        About
+                      </a>
+                      <a
+                        className='text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                        href='/login'
+                      >
+                        Login
+                      </a>
+                      <div
+                        onClick={() =>
+                          window.open('https://github.com/cevo-public/cov-spectrum-website', '_blank')
+                        }
+                      >
+                        <AiOutlineGithub
+                          className='fill-current hover:text-gray-500 rounded-xl filter shadow-xl cursor-pointer'
+                          size={'1.5em'}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className='block'>
-                <div className='ml-4 flex items-center md:ml-6'></div>
-              </div>
-              <div className='-mr-2 flex md:hidden'>
-                <button className='text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none'>
-                  <svg
-                    width={20}
-                    height={20}
-                    fill='currentColor'
-                    className='h-8 w-8'
-                    viewBox='0 0 1792 1792'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z'></path>
-                  </svg>
-                </button>
+                <div className='block'>
+                  <div className='ml-4 flex items-center md:ml-6'></div>
+                </div>
+                <div className='-mr-2 flex md:hidden'>
+                  <button className='text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none'>
+                    <svg
+                      width={20}
+                      height={20}
+                      fill='currentColor'
+                      className='h-8 w-8'
+                      viewBox='0 0 1792 1792'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z'></path>
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='md:hidden'>
-            <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-              <a
-                className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-                href='/#'
-              >
-                Home
-              </a>
-              <a
-                className='text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium'
-                href='/#'
-              >
-                Gallery
-              </a>
-              <a
-                className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-                href='/#'
-              >
-                Content
-              </a>
-              <a
-                className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-                href='/#'
-              >
-                Contact
-              </a>
+            <div className='md:hidden'>
+              <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+                <a
+                  className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+                  href='/#'
+                >
+                  Home
+                </a>
+                <a
+                  className='text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium'
+                  href='/#'
+                >
+                  Gallery
+                </a>
+                <a
+                  className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+                  href='/#'
+                >
+                  Content
+                </a>
+                <a
+                  className='text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+                  href='/#'
+                >
+                  Contact
+                </a>
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
+        </div>
+        <div id='navbar-bottom'>
+          <nav className='bg-blue '>
+            <div className='max-w-9xl mx-auto px-8'>
+              <div className='flex items-center justify-between h-12'>
+                <div className='w-full flex items-center z-20'>
+                  <HeaderCountrySelect />
+                  <HeaderDateRangeSelect />
+                  <HeaderSamplingStrategySelect />
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </>
 
@@ -275,3 +289,5 @@ export const Header = () => {
     // </Wrapper>
   );
 };
+
+export default Header;
