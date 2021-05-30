1ce2c54674521dd2ecb56f77e2245e4cb7870865
Philippe Voinov <philippevoinov@gmail.com>
2021-05-30 13:36:45 +0200

    Fix: Order of export options changes

--- src/components/CombinedExport/ExportButton.tsx
+++ src/components/CombinedExport/ExportButton.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { ExportManagerContext, RegisteredExport } from './ExportManager';
+import { sortBy } from 'lodash';
 
 export const ExportButton = () => {
   const [items, setItems] = useState<RegisteredExport[]>([]);
@@ -8,7 +9,10 @@ export const ExportButton = () => {
   const exportManager = useContext(ExportManagerContext);
 
   return (
-    <DropdownButton title='Export' onClick={() => setItems([...exportManager.getRegistered()])}>
+    <DropdownButton
+      title='Export'
+      onClick={() => setItems(sortBy([...exportManager.getRegistered()], ({ label }) => label))}
+    >
       {items.map(({ label, doExport }) => (
         <Dropdown.Item key={label} onClick={doExport}>
           {label}
