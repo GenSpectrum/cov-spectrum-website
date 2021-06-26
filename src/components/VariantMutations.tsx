fb8a2699806a0e12e47f61630e8b96b8bc7b24b4
Chaoran Chen <mail@chaoran-chen.de>
2021-06-26 23:54:41 +0200

    Show common nucleotide mutations of a pangolin lineage

--- src/components/VariantMutations.tsx
+++ src/components/VariantMutations.tsx
@@ -3,6 +3,7 @@ import { Country } from '../services/api-types';
 import { DateRange, dateRangeToDates, getInformationOfPangolinLineage } from '../services/api';
 import styled from 'styled-components';
 import { MutationName } from './MutationName';
+import { ExternalLink } from './ExternalLink';
 
 export interface Props {
   region?: string;
@@ -22,13 +23,21 @@ const MutationEntry = styled.li`
 `;
 
 export const VariantMutations = ({ region, country, pangolinLineage, dateRange }: Props) => {
-  const [data, setData] = useState<
-    {
+  const [data, setData] = useState<{
+    aa: {
       mutation: string;
       count: number;
       proportion: number;
-    }[]
-  >([]);
+    }[];
+    nuc: {
+      mutation: string;
+      count: number;
+      proportion: number;
+    }[];
+  }>({
+    aa: [],
+    nuc: [],
+  });
   useEffect(() => {
     const { dateFrom, dateTo } = dateRangeToDates(dateRange);
     getInformationOfPangolinLineage({
@@ -37,22 +46,42 @@ export const VariantMutations = ({ region, country, pangolinLineage, dateRange }
       country,
       dateFrom,
       dateTo,
-    }).then(({ commonMutations }) => {
+    }).then(({ commonMutations, commonNucMutations }) => {
       commonMutations.sort((a, b) => b.count - a.count);
-      setData(commonMutations);
+      commonNucMutations.sort((a, b) => b.count - a.count);
+      setData({
+        aa: commonMutations,
+        nuc: commonNucMutations,
+      });
     });
   }, [pangolinLineage, region, country, dateRange]);
 
   return (
     <>
-      <div>The following mutations are common to this lineage:</div>
+      <div>The following (amino acid) mutations are common to this lineage:</div>
       <MutationList className='list-disc'>
-        {data
+        {data.aa
           .sort((a, b) => b.count - a.count)
           .map(({ mutation, proportion }) => {
             return (
               <MutationEntry key={mutation}>
                 <MutationName mutation={mutation} /> ({(proportion * 100).toFixed(2)}%)
+              </MutationEntry>
+            );
+          })}
+      </MutationList>
+      <div className='mt-4'>
+        The following nucleotide mutations are common to this lineage (
+        <ExternalLink url='https://github.com/W-L/ProblematicSites_SARS-CoV2'>problematic sites</ExternalLink>{' '}
+        and leading and tailing deletions are excluded):
+      </div>
+      <MutationList className='list-disc'>
+        {data.nuc
+          .sort((a, b) => b.count - a.count)
+          .map(({ mutation, proportion }) => {
+            return (
+              <MutationEntry key={mutation}>
+                {mutation} ({(proportion * 100).toFixed(2)}%)
               </MutationEntry>
             );
           })}
