0b538a099ef3e513db1066a50568ad63a18fc66c
ningxie <ning.xie@uzh.ch>
2021-08-31 14:54:33 +0200

    Remove refetch calls

--- src/widgets/SequencingRepresentativenessPlot.tsx
+++ src/widgets/SequencingRepresentativenessPlot.tsx
@@ -88,7 +88,6 @@ export const SequencingRepresentativenessPlot = React.memo(({ selector }: Props)
   const {
     data: caseCounts,
     isFetching: isCCFetching,
-    refetch: cCRefetch,
     isError: isCCError,
     error: cCError,
     isLoading: isCCLoading,
@@ -122,7 +121,6 @@ export const SequencingRepresentativenessPlot = React.memo(({ selector }: Props)
 
   const {
     isFetching: isSCFetching,
-    refetch: sCRefetch,
     isError: isSCError,
     error: sCError,
     isLoading: isSCLoading,
