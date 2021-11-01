b4a0472777448977819dfadc9821c450ab966a0c
Michael Yared <m@Michaels-MacBook-Pro-2.local>
2021-11-02 00:53:27 +0100

    format

--- src/widgets/Metrics.tsx
+++ src/widgets/Metrics.tsx
@@ -131,7 +131,7 @@ const Metric = ({
           <PercentWrapper className='self-end'>{showPercent && '' + showPercent + '%'}</PercentWrapper>
         </div>
         <MetricTitleWrapper id='metric-title'>
-          <p className='w-full overflow-clip'>{title + ' '}</p>
+          <p className='w-auto overflow-clip'>{title + ' '}</p>
           <IconWrapper id='info-wrapper'>
             <BiHelpCircle />
           </IconWrapper>
