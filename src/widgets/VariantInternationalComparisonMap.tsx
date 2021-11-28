48b4395d300476eb73faefb538cecb9bdb35c457
TKGZ <tiankongguangzi@gmail.com>
2021-11-28 21:41:13 +0100

    format

--- src/widgets/VariantInternationalComparisonMap.tsx
+++ src/widgets/VariantInternationalComparisonMap.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import { ChartAndMetricsWrapper, Wrapper } from './common';
 import { CountryDateCountSampleDataset } from '../data/sample/CountryDateCountSampleDataset';
 import { Utils } from '../services/Utils';
