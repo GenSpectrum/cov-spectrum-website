e9c68b11df1af0d9391ca986970a9514a854f2de
Gautier <gautier@nexus.ethz.ch>
2022-02-28 15:29:57 +0100

    run prettier

--- src/helpers/max-y-axis.ts
+++ src/helpers/max-y-axis.ts
@@ -1,14 +1,14 @@
 // return maximum value to display in Y axis of a chart
 export function maxYAxis(yMax: number, resultIfNotZero?: number, resultIfZero?: number): number {
   if (yMax === 0) {
-    if (typeof resultIfZero !== "undefined") {
-      return resultIfZero
+    if (typeof resultIfZero !== 'undefined') {
+      return resultIfZero;
     } else {
-      return 1
+      return 1;
     }
-  } else if (typeof resultIfNotZero !== "undefined") {
+  } else if (typeof resultIfNotZero !== 'undefined') {
     return resultIfNotZero;
   } else {
-    return yMax
+    return yMax;
   }
 }
