074167e08aed3cf09ebe3c4f6120257b676734b2
Anastasia Escher <escher@nexus.ethz.ch>
2022-04-13 14:54:47 +0200

    add dropdown for selecting geo

--- src/components/PlaceSelect.tsx
+++ src/components/PlaceSelect.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import ReactCountryFlag from 'react-country-flag';
 import { LocationService } from '../services/LocationService';
 import { useQuery } from '../helpers/query-hook';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export interface Props {
   id?: string;
@@ -13,22 +15,56 @@ export interface Props {
 
 export const PlaceSelect = ({ id, selected, onSelect, onMenuToggle }: Props) => {
   const places: string[] = useQuery(() => LocationService.getAllLocationNames(), []).data ?? [];
+  const [geoOption, setGeoOption] = useState<string>('Countries');
+
+  const world: string[] = ['World'];
+  const regions: string[] = ['Africa', 'Asia', 'Europe', 'North America', 'Oceania', 'South America'];
+
+  let filteredPlaces: string[] = [];
+
+  function handleGeoOptionChange(e: React.MouseEvent<HTMLElement>, option: string) {
+    e.preventDefault();
+    setGeoOption(option);
+  }
+
+  switch (geoOption) {
+    case 'Regions':
+      filteredPlaces = places.filter(place => regions.includes(place));
+      break;
+    case 'World':
+      filteredPlaces = places.filter(place => world.includes(place));
+      break;
+    case 'Countries':
+      filteredPlaces = places.filter(place => !world.includes(place) && !regions.includes(place));
+  }
 
   return (
-    <>
+    <div className='flex space-x-4'>
       <Typeahead
         id={id ? id : 'typeahead'}
         selected={selected ? [selected] : []}
         placeholder='Select country/region'
         onChange={selected => onSelect(selected.length === 1 ? selected[0] : undefined)}
         onMenuToggle={onMenuToggle}
-        options={places}
+        options={filteredPlaces}
         maxResults={places.length + 100} // +100 is just to be safe...
         paginate={false}
       >
         {getFlag(selected)}
       </Typeahead>
-    </>
+
+      <DropdownButton id='dropdown-basic-button' title={geoOption}>
+        <Dropdown.Item onClick={(e: React.MouseEvent<HTMLElement>) => handleGeoOptionChange(e, 'World')}>
+          World
+        </Dropdown.Item>
+        <Dropdown.Item onClick={(e: React.MouseEvent<HTMLElement>) => handleGeoOptionChange(e, 'Regions')}>
+          Regions
+        </Dropdown.Item>
+        <Dropdown.Item onClick={(e: React.MouseEvent<HTMLElement>) => handleGeoOptionChange(e, 'Countries')}>
+          Countries
+        </Dropdown.Item>
+      </DropdownButton>
+    </div>
   );
 };
 
