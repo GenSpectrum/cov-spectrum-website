39e3a718d0de99770b9cfae0b2e4ca2edd098f03
Anastasia Escher <escher@nexus.ethz.ch>
2022-04-27 16:55:59 +0200

    add copy to clepboard, fixe size of the plots and add scrolling for smaller screens

--- src/components/SvgVennDiagram2.tsx
+++ src/components/SvgVennDiagram2.tsx
@@ -9,12 +9,24 @@ import { ReferenceGenomeService } from '../services/ReferenceGenomeService';
 import Loader from './Loader';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Slider from 'rc-slider';
+import { useResizeDetector } from 'react-resize-detector';
+import styled from 'styled-components';
 
 export interface Props {
   selectors: LapisSelector[];
 }
 
 export const SvgVennDiagram2 = ({ selectors }: Props) => {
+  const { width, ref } = useResizeDetector<HTMLDivElement>();
+
+  interface PlotContProps {
+    width: number;
+  }
+
+  const PlotContainer = styled.div<PlotContProps>`
+    overflow: ${props => (props.width < 500 ? 'scroll' : 'auto')};
+  `;
+
   const [minProportion, setMinProportion] = useState(0.5);
   console.log('selectors', selectors);
   const res = useQuery(
@@ -197,63 +209,83 @@ export const SvgVennDiagram2 = ({ selectors }: Props) => {
         </OverlayTrigger>{' '}
         of the samples of the variant have the mutation.
       </div>
+      <p>
+        * Clicking to the number of the mutations inside the diagram will copy the mutations to clipboard.{' '}
+      </p>
       <br />
-      <svg
-        width='500'
-        height='500'
-        version='1.1'
-        id='Layer_1'
-        x='0px'
-        y='0px'
-        viewBox='0 0 910 577'
-        xmlns='http://www.w3.org/2000/svg'
-        className='svgPlot'
-      >
-        <g>
-          <path
-            className='st0'
-            d='M562.05,172.22c15.54,36.74,23.42,75.75,23.42,115.97s-7.88,79.24-23.42,115.97 c-15.01,35.48-36.49,67.34-63.85,94.7c-9.07,9.07-18.64,17.49-28.67,25.25c44.2,28.87,96.96,45.67,153.58,45.67 c155.27,0,281.59-126.32,281.59-281.59S778.39,6.61,623.12,6.61c-56.62,0-109.38,16.8-153.58,45.67 c10.03,7.76,19.6,16.18,28.67,25.25C525.57,104.88,547.05,136.75,562.05,172.22z'
-          />
-          <path
-            className='st0'
-            d='M348.61,404.17c-15.54-36.74-23.42-75.75-23.42-115.97c0-40.22,7.88-79.23,23.42-115.97 c15.01-35.48,36.49-67.34,63.85-94.7c9.07-9.07,18.64-17.49,28.67-25.25c-44.2-28.87-96.96-45.67-153.58-45.67 C132.27,6.61,5.95,132.93,5.95,288.19s126.32,281.59,281.59,281.59c56.62,0,109.38-16.8,153.58-45.67 c-10.03-7.76-19.6-16.18-28.67-25.25C385.09,471.5,363.61,439.64,348.61,404.17z'
-          />
-          <path
-            className='st0'
-            d='M569.13,288.19c0-92.45-44.78-174.63-113.8-226c-69.01,51.37-113.8,133.55-113.8,226s44.78,174.63,113.8,226 C524.34,462.83,569.13,380.64,569.13,288.19z'
-          />
-        </g>
-        <text
-          transform='matrix(1 0 0 1 154.6328 294.3799)'
-          className='st1 st2 svgPlotNumber'
-          data-for='variant1'
-          data-tip
+      <PlotContainer ref={ref} width={width ? width : 600}>
+        <svg
+          width='370'
+          height='370'
+          version='1.1'
+          id='Layer_1'
+          x='0px'
+          y='0px'
+          viewBox='0 0 910 577'
+          xmlns='http://www.w3.org/2000/svg'
+          className='svgPlot'
         >
-          {allMutations.variant1Mutations.length}
-        </text>
-        <text
-          transform='matrix(1 0 0 1 427.6328 294.3799)'
-          className='st1 st2 svgPlotNumber'
-          data-for='shared'
-          data-tip
-        >
-          {allMutations.shared.length}
-        </text>
-        <text
-          transform='matrix(1 0 0 1 712.6328 294.3799)'
-          className='st1 st2 svgPlotNumber'
-          data-for='variant2'
-          data-tip
-        >
-          {allMutations.variant2Mutations.length}
-        </text>
-        <text x='8.785' y='37.948' className='svgPlotText'>
-          {variants ? variants[0].pangoLineage : null}
-        </text>
-        <text x='777.125' y='31.79' className='svgPlotText'>
-          {variants ? variants[1].pangoLineage : null}
-        </text>
-      </svg>
+          <g>
+            <path
+              className='st0'
+              d='M562.05,172.22c15.54,36.74,23.42,75.75,23.42,115.97s-7.88,79.24-23.42,115.97 c-15.01,35.48-36.49,67.34-63.85,94.7c-9.07,9.07-18.64,17.49-28.67,25.25c44.2,28.87,96.96,45.67,153.58,45.67 c155.27,0,281.59-126.32,281.59-281.59S778.39,6.61,623.12,6.61c-56.62,0-109.38,16.8-153.58,45.67 c10.03,7.76,19.6,16.18,28.67,25.25C525.57,104.88,547.05,136.75,562.05,172.22z'
+            />
+            <path
+              className='st0'
+              d='M348.61,404.17c-15.54-36.74-23.42-75.75-23.42-115.97c0-40.22,7.88-79.23,23.42-115.97 c15.01-35.48,36.49-67.34,63.85-94.7c9.07-9.07,18.64-17.49,28.67-25.25c-44.2-28.87-96.96-45.67-153.58-45.67 C132.27,6.61,5.95,132.93,5.95,288.19s126.32,281.59,281.59,281.59c56.62,0,109.38-16.8,153.58-45.67 c-10.03-7.76-19.6-16.18-28.67-25.25C385.09,471.5,363.61,439.64,348.61,404.17z'
+            />
+            <path
+              className='st0'
+              d='M569.13,288.19c0-92.45-44.78-174.63-113.8-226c-69.01,51.37-113.8,133.55-113.8,226s44.78,174.63,113.8,226 C524.34,462.83,569.13,380.64,569.13,288.19z'
+            />
+          </g>
+          <text
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${allMutations.variant1Mutations.length ? allMutations.variant1Mutations.join(', ') : ''}`
+              );
+            }}
+            transform='matrix(1 0 0 1 154.6328 294.3799)'
+            className='st1 st2 svgPlotNumber'
+            data-for='variant1'
+            data-tip
+          >
+            {allMutations.variant1Mutations.length}
+          </text>
+          <text
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${allMutations.shared.length ? allMutations.shared.join(', ') : ''}`
+              );
+            }}
+            transform='matrix(1 0 0 1 427.6328 294.3799)'
+            className='st1 st2 svgPlotNumber'
+            data-for='shared'
+            data-tip
+          >
+            {allMutations.shared.length}
+          </text>
+          <text
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `${allMutations.variant2Mutations.length ? allMutations.variant2Mutations.join(', ') : ''}`
+              );
+            }}
+            transform='matrix(1 0 0 1 712.6328 294.3799)'
+            className='st1 st2 svgPlotNumber'
+            data-for='variant2'
+            data-tip
+          >
+            {allMutations.variant2Mutations.length}
+          </text>
+          <text x='8.785' y='37.948' className='svgPlotText'>
+            {variants ? variants[0].pangoLineage : null}
+          </text>
+          <text x='777.125' y='31.79' className='svgPlotText'>
+            {variants ? variants[1].pangoLineage : null}
+          </text>
+        </svg>
+      </PlotContainer>
       <>
         <ReactTooltip id='variant1' key='variant1'>
           {allMutations.variant1Mutations.length
--- src/components/SvgVennDiagram3.tsx
+++ src/components/SvgVennDiagram3.tsx
@@ -9,12 +9,24 @@ import { ReferenceGenomeService } from '../services/ReferenceGenomeService';
 import Loader from './Loader';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Slider from 'rc-slider';
+import { useResizeDetector } from 'react-resize-detector';
+import styled from 'styled-components';
 
 export interface Props {
   selectors: LapisSelector[];
 }
 
 export const SvgVennDiagram3 = ({ selectors }: Props) => {
+  const { width, ref } = useResizeDetector<HTMLDivElement>();
+
+  interface PlotContProps {
+    width: number;
+  }
+
+  const PlotContainer = styled.div<PlotContProps>`
+    overflow: ${props => (props.width < 500 ? 'scroll' : 'auto')};
+  `;
+
   const [minProportion, setMinProportion] = useState(0.5);
   console.log('selectors', selectors);
   const res = useQuery(
@@ -176,130 +188,178 @@ export const SvgVennDiagram3 = ({ selectors }: Props) => {
             </OverlayTrigger>{' '}
             of the samples of the variant have the mutation.
           </div>
+          <p>
+            * Clicking to the number of the mutations inside the diagram will copy the mutations to clipboard.{' '}
+          </p>
           <br />
-          <svg
-            version='1.1'
-            id='Layer_1'
-            xmlns='http://www.w3.org/2000/svg'
-            x='0px'
-            y='0px'
-            viewBox='0 0 1178.62 1127.39'
-            className='svgPlot3'
-          >
-            <g>
-              <path
-                className='st0'
-                d='M918.61,617.78c-14.38,21.28-30.89,41.3-49.08,59.48c-18.19,18.19-38.2,34.7-59.48,49.08
+          <PlotContainer ref={ref} width={width ? width : 600}>
+            <svg
+              width='570'
+              height='570'
+              version='1.1'
+              id='Layer_1'
+              xmlns='http://www.w3.org/2000/svg'
+              x='0px'
+              y='0px'
+              viewBox='0 0 1178.62 1127.39'
+              className='svgPlot3'
+            >
+              <g>
+                <path
+                  className='st0'
+                  d='M918.61,617.78c-14.38,21.28-30.89,41.3-49.08,59.48c-18.19,18.19-38.2,34.7-59.48,49.08
 		c-6.31,4.27-12.76,8.35-19.31,12.25c-0.65,52.44-11.26,103.32-31.56,151.32c-10.08,23.82-22.54,46.79-37.06,68.28
 		c-14.38,21.28-30.89,41.3-49.08,59.48c-18.19,18.19-38.2,34.7-59.48,49.08c-3.37,2.28-6.78,4.5-10.22,6.67
 		c53.35,28.12,114.08,44.05,178.46,44.05c211.75,0,384.02-172.27,384.02-384.02c0-136.36-71.45-256.35-178.87-324.5
 		c-1.88,48.64-12.37,95.85-31.28,140.55C945.59,573.32,933.12,596.29,918.61,617.78z'
-              />
-              <path
-                className='st0'
-                d='M497.57,1017.67c-18.19-18.19-34.7-38.2-49.08-59.48c-14.52-21.49-26.99-44.46-37.06-68.28
+                />
+                <path
+                  className='st0'
+                  d='M497.57,1017.67c-18.19-18.19-34.7-38.2-49.08-59.48c-14.52-21.49-26.99-44.46-37.06-68.28
 		c-20.3-47.99-30.91-98.87-31.56-151.32c-6.55-3.9-13-7.99-19.31-12.25c-21.28-14.38-41.3-30.89-59.48-49.08
 		c-18.19-18.19-34.7-38.2-49.08-59.48c-14.52-21.49-26.99-44.46-37.06-68.28c-18.91-44.7-29.4-91.91-31.28-140.55
 		C76.23,477.1,4.79,597.08,4.79,733.45c0,211.75,172.27,384.02,384.02,384.02c64.38,0,125.11-15.93,178.46-44.05
 		c-3.44-2.17-6.85-4.39-10.22-6.67C535.77,1052.37,515.75,1035.86,497.57,1017.67z'
-              />
-              <path
-                className='st0'
-                d='M772.52,748.82c-10.05,5.3-20.32,10.17-30.76,14.59c-49.56,20.96-102.2,31.59-156.46,31.59
+                />
+                <path
+                  className='st0'
+                  d='M772.52,748.82c-10.05,5.3-20.32,10.17-30.76,14.59c-49.56,20.96-102.2,31.59-156.46,31.59
 		c-54.26,0-106.9-10.63-156.46-31.59c-10.43-4.41-20.7-9.28-30.76-14.59c5.28,133.57,79.12,249.82,187.22,314.47
 		C693.4,998.64,767.24,882.39,772.52,748.82z'
-              />
-              <path
-                className='st0'
-                d='M388.81,331.49c54.26,0,106.9,10.63,156.46,31.59c13.66,5.78,27.05,12.35,40.03,19.64
+                />
+                <path
+                  className='st0'
+                  d='M388.81,331.49c54.26,0,106.9,10.63,156.46,31.59c13.66,5.78,27.05,12.35,40.03,19.64
 		c12.99-7.29,26.37-13.86,40.03-19.64c49.56-20.96,102.2-31.59,156.46-31.59c54.26,0,106.9,10.63,156.46,31.59
 		c10.43,4.41,20.7,9.28,30.76,14.59C960.92,173.01,791.9,9.01,585.3,9.01s-375.62,164-383.71,368.65
 		c10.05-5.3,20.32-10.17,30.76-14.59C281.9,342.12,334.55,331.49,388.81,331.49z'
-              />
-              <path
-                className='st0'
-                d='M603.33,393.47c3.44,2.17,6.85,4.39,10.22,6.67c21.28,14.38,41.3,30.89,59.48,49.08
+                />
+                <path
+                  className='st0'
+                  d='M603.33,393.47c3.44,2.17,6.85,4.39,10.22,6.67c21.28,14.38,41.3,30.89,59.48,49.08
 		c18.19,18.19,34.7,38.2,49.08,59.48c14.52,21.49,26.99,44.46,37.06,68.28c18.91,44.7,29.4,91.91,31.28,140.55
 		c106.01-67.26,176.99-185,178.83-319.15c-55.48-31.17-119.45-48.96-187.49-48.96C717.41,349.43,656.68,365.35,603.33,393.47z'
-              />
-              <path
-                className='st0'
-                d='M380.15,717.53c1.88-48.64,12.37-95.85,31.28-140.55c10.07-23.82,22.54-46.79,37.06-68.28
+                />
+                <path
+                  className='st0'
+                  d='M380.15,717.53c1.88-48.64,12.37-95.85,31.28-140.55c10.07-23.82,22.54-46.79,37.06-68.28
 		c14.38-21.28,30.89-41.3,49.08-59.48c18.19-18.19,38.2-34.7,59.48-49.08c3.37-2.28,6.78-4.5,10.22-6.67
 		c-53.35-28.12-114.08-44.05-178.46-44.05c-68.04,0-132,17.79-187.49,48.96C203.16,532.53,274.13,650.27,380.15,717.53z'
-              />
-              <path
-                className='st0'
-                d='M397.81,728.1c55.48,31.17,119.45,48.96,187.49,48.96c68.04,0,132-17.79,187.49-48.96
+                />
+                <path
+                  className='st0'
+                  d='M397.81,728.1c55.48,31.17,119.45,48.96,187.49,48.96c68.04,0,132-17.79,187.49-48.96
 		c-1.89-137.77-76.7-258.23-187.49-324.48C474.51,469.86,399.71,590.33,397.81,728.1z'
-              />
-            </g>
-            <text
-              transform='matrix(1 0 0 1 548.3361 206.3148)'
-              className='st1 st2 svgPlotNumber'
-              data-for='variant1'
-              data-tip
-            >
-              {allMutations.variant1Mutations.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 548.3361 620.9984)'
-              className='st1 st2 svgPlotNumber'
-              data-for='shared'
-              data-tip
-            >
-              {allMutations.shared.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 548.3361 935.4288)'
-              className='st1 st2 svgPlotNumber'
-              data-for='shared2and3'
-              data-tip
-            >
-              {allMutations.shared2and3.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 307.7284 511.6313)'
-              className='st1 st2 svgPlotNumber'
-              data-for='shared1and2'
-              data-tip
-            >
-              {allMutations.shared1and2.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 808.9943 511.6313)'
-              className='st1 st2 svgPlotNumber'
-              data-for='shared1and3'
-              data-tip
-            >
-              {allMutations.shared1and3.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 963.0196 823.3275)'
-              className='st1 st2 svgPlotNumber'
-              data-for='variant3'
-              data-tip
-            >
-              {allMutations.variant3Mutations.length}
-            </text>
-            <text
-              transform='matrix(1 0 0 1 182.8677 823.3275)'
-              className='st1 st2 svgPlotNumber'
-              data-for='variant2'
-              data-tip
-            >
-              {allMutations.variant2Mutations.length}
-            </text>
-            <text className='svgPlotText' x='857.256' y='83.287'>
-              {variants ? variants[0].pangoLineage : null}
-            </text>
-            <text className='svgPlotText' x='12.812' y='1069.66'>
-              {variants ? variants[1].pangoLineage : null}
-            </text>
-            <text className='svgPlotText' x='1014.25' y='1067.59'>
-              {variants ? variants[2].pangoLineage : null}
-            </text>
-          </svg>
+                />
+              </g>
+              <text
+                transform='matrix(1 0 0 1 548.3361 206.3148)'
+                className='st1 st2 svgPlotNumber'
+                data-for='variant1'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${
+                      allMutations.variant1Mutations.length ? allMutations.variant1Mutations.join(', ') : ''
+                    }`
+                  );
+                }}
+              >
+                {allMutations.variant1Mutations.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 548.3361 620.9984)'
+                className='st1 st2 svgPlotNumber'
+                data-for='shared'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${allMutations.shared.length ? allMutations.shared.join(', ') : ''}`
+                  );
+                }}
+              >
+                {allMutations.shared.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 548.3361 935.4288)'
+                className='st1 st2 svgPlotNumber'
+                data-for='shared2and3'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${allMutations.shared2and3.length ? allMutations.shared2and3.join(', ') : ''}`
+                  );
+                }}
+              >
+                {allMutations.shared2and3.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 307.7284 511.6313)'
+                className='st1 st2 svgPlotNumber'
+                data-for='shared1and2'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${allMutations.shared1and2.length ? allMutations.shared1and2.join(', ') : ''}`
+                  );
+                }}
+              >
+                {allMutations.shared1and2.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 808.9943 511.6313)'
+                className='st1 st2 svgPlotNumber'
+                data-for='shared1and3'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${allMutations.shared1and3.length ? allMutations.shared1and3.join(', ') : ''}`
+                  );
+                }}
+              >
+                {allMutations.shared1and3.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 963.0196 823.3275)'
+                className='st1 st2 svgPlotNumber'
+                data-for='variant3'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${
+                      allMutations.variant3Mutations.length ? allMutations.variant3Mutations.join(', ') : ''
+                    }`
+                  );
+                }}
+              >
+                {allMutations.variant3Mutations.length}
+              </text>
+              <text
+                transform='matrix(1 0 0 1 182.8677 823.3275)'
+                className='st1 st2 svgPlotNumber'
+                data-for='variant2'
+                data-tip
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${
+                      allMutations.variant2Mutations.length ? allMutations.variant2Mutations.join(', ') : ''
+                    }`
+                  );
+                }}
+              >
+                {allMutations.variant2Mutations.length}
+              </text>
+              <text className='svgPlotText' x='857.256' y='83.287'>
+                {variants ? variants[0].pangoLineage : null}
+              </text>
+              <text className='svgPlotText' x='12.812' y='1069.66'>
+                {variants ? variants[1].pangoLineage : null}
+              </text>
+              <text className='svgPlotText' x='1014.25' y='1067.59'>
+                {variants ? variants[2].pangoLineage : null}
+              </text>
+            </svg>
+          </PlotContainer>
 
           <>
             <ReactTooltip id='variant1' key='variant1'>
