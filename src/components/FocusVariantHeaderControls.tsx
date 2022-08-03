2b40c4bdeddef04b5a76ab39c29c9f7dda38bd9a
Chaoran Chen <mail@chaoran-chen.de>
2022-08-03 11:38:24 +0200

    Fix format

--- src/components/FocusVariantHeaderControls.tsx
+++ src/components/FocusVariantHeaderControls.tsx
@@ -33,91 +33,92 @@ const integrations: Integration[] = [
   new NextcladeIntegration(),
 ];
 
-export const FocusVariantHeaderControls = React.memo(({ selector }: Props): JSX.Element => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const showDropdownFunc = (_: any) => {
-    setShowDropdown(!showDropdown);
-  };
-  const hideDropdownFunc = (_: any) => {
-    setShowDropdown(false);
-  };
+export const FocusVariantHeaderControls = React.memo(
+  ({ selector }: Props): JSX.Element => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const showDropdownFunc = (_: any) => {
+      setShowDropdown(!showDropdown);
+    };
+    const hideDropdownFunc = (_: any) => {
+      setShowDropdown(false);
+    };
 
-  // Sequence list download
-  // If the open version is used, all the metadata will be downloaded. If GISAID is used, only the contributors
-  // will be downloaded.
-  const getLinkFunc = sequenceDataSource === 'open' ? getCsvLinkToDetails : getCsvLinkToContributors;
-  const linkToListPromise = useDeepCompareMemo(() => getLinkFunc(selector), [selector]);
-  const { data: listLink } = useAsync({ promise: linkToListPromise });
+    // Sequence list download
+    // If the open version is used, all the metadata will be downloaded. If GISAID is used, only the contributors
+    // will be downloaded.
+    const getLinkFunc = sequenceDataSource === 'open' ? getCsvLinkToDetails : getCsvLinkToContributors;
+    const linkToListPromise = useDeepCompareMemo(() => getLinkFunc(selector), [selector]);
+    const { data: listLink } = useAsync({ promise: linkToListPromise });
 
-  // FASTA download
-  const orderAndLimit: OrderAndLimitConfig = {
-    orderBy: 'random',
-    limit: 10000,
-  };
-  const linkToFastaPromise = useDeepCompareMemo(
-    () => getLinkToFasta(false, selector, orderAndLimit),
-    [selector]
-  );
-  const { data: fastaLink } = useAsync({ promise: linkToFastaPromise });
-  const linkToAlignedFastaPromise = useDeepCompareMemo(
-    () => getLinkToFasta(true, selector, orderAndLimit),
-    [selector]
-  );
-  const { data: alignedFastaLink } = useAsync({ promise: linkToAlignedFastaPromise });
+    // FASTA download
+    const orderAndLimit: OrderAndLimitConfig = {
+      orderBy: 'random',
+      limit: 10000,
+    };
+    const linkToFastaPromise = useDeepCompareMemo(() => getLinkToFasta(false, selector, orderAndLimit), [
+      selector,
+    ]);
+    const { data: fastaLink } = useAsync({ promise: linkToFastaPromise });
+    const linkToAlignedFastaPromise = useDeepCompareMemo(
+      () => getLinkToFasta(true, selector, orderAndLimit),
+      [selector]
+    );
+    const { data: alignedFastaLink } = useAsync({ promise: linkToAlignedFastaPromise });
 
-  const listLink2: string | undefined =
-    listLink &&
-    listLink.replace('contributors', 'gisaid-epi-isl').replace('&downloadAsFile=true&dataFormat=csv', '') +
-      '&orderBy=random';
+    const listLink2: string | undefined =
+      listLink &&
+      listLink.replace('contributors', 'gisaid-epi-isl').replace('&downloadAsFile=true&dataFormat=csv', '') +
+        '&orderBy=random';
 
-  return (
-    <>
-      <Dropdown as={ButtonGroup} className='mr-2 mt-3' size='sm'>
-        <ExternalLink url={listLink ?? ''}>
-          <Button variant='secondary' size='sm'>
-            Sequence list <FaDownload className='inline-block ml-1' />
-          </Button>
-        </ExternalLink>
+    return (
+      <>
+        <Dropdown as={ButtonGroup} className='mr-2 mt-3' size='sm'>
+          <ExternalLink url={listLink ?? ''}>
+            <Button variant='secondary' size='sm'>
+              Sequence list <FaDownload className='inline-block ml-1' />
+            </Button>
+          </ExternalLink>
 
-        <Dropdown.Toggle split variant='secondary' />
-        <Dropdown.Menu>
-          {sequenceDataSource === 'gisaid' && (
-            <ExternalLink url={listLink2 ?? ''}>
-              <Dropdown.Item as={Button}>GISAID list</Dropdown.Item>
-            </ExternalLink>
-          )}
-          {sequenceDataSource === 'open' && (
-            <>
-              <ExternalLink url={fastaLink ?? ''}>
-                <Dropdown.Item as={Button}>FASTA</Dropdown.Item>
+          <Dropdown.Toggle split variant='secondary' />
+          <Dropdown.Menu>
+            {sequenceDataSource === 'gisaid' && (
+              <ExternalLink url={listLink2 ?? ''}>
+                <Dropdown.Item as={Button}>GISAID list</Dropdown.Item>
               </ExternalLink>
-              <ExternalLink url={alignedFastaLink ?? ''}>
-                <Dropdown.Item as={Button}>FASTA (aligned)</Dropdown.Item>
-              </ExternalLink>
-            </>
-          )}
-        </Dropdown.Menu>
-      </Dropdown>
+            )}
+            {sequenceDataSource === 'open' && (
+              <>
+                <ExternalLink url={fastaLink ?? ''}>
+                  <Dropdown.Item as={Button}>FASTA</Dropdown.Item>
+                </ExternalLink>
+                <ExternalLink url={alignedFastaLink ?? ''}>
+                  <Dropdown.Item as={Button}>FASTA (aligned)</Dropdown.Item>
+                </ExternalLink>
+              </>
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
 
-      <DropdownButton
-        as={ButtonGroup}
-        title='Other websites'
-        variant='secondary'
-        size='sm'
-        className='mr-2 mt-3'
-        onMouseEnter={showDropdownFunc}
-        onMouseLeave={hideDropdownFunc}
-        show={showDropdown}
-      >
-        {integrations.map(
-          integration =>
-            integration.isAvailable(selector) && (
-              <Dropdown.Item key={integration.name} onClick={() => integration.open(selector)}>
-                {integration.name}
-              </Dropdown.Item>
-            )
-        )}
-      </DropdownButton>
-    </>
-  );
-});
+        <DropdownButton
+          as={ButtonGroup}
+          title='Other websites'
+          variant='secondary'
+          size='sm'
+          className='mr-2 mt-3'
+          onMouseEnter={showDropdownFunc}
+          onMouseLeave={hideDropdownFunc}
+          show={showDropdown}
+        >
+          {integrations.map(
+            integration =>
+              integration.isAvailable(selector) && (
+                <Dropdown.Item key={integration.name} onClick={() => integration.open(selector)}>
+                  {integration.name}
+                </Dropdown.Item>
+              )
+          )}
+        </DropdownButton>
+      </>
+    );
+  }
+);
