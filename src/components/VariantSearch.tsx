dac071095af888e2210642ef5a6e1725cf779d8f
Chaoran Chen <mail@chaoran-chen.de>
2022-08-10 16:16:37 +0200

    Add button to move a variant search to the top in the baseline mode

--- src/components/VariantSearch.tsx
+++ src/components/VariantSearch.tsx
@@ -3,6 +3,8 @@ import { Button, ButtonVariant } from '../helpers/ui';
 import React, { useEffect, useState } from 'react';
 import { VariantSearchField } from './VariantSearchField';
 import { AnalysisMode } from '../data/AnalysisMode';
+import { FaArrowUp } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
 
 type Props = {
   currentSelection?: VariantSelector[];
@@ -87,6 +89,17 @@ export const VariantSearch = ({ currentSelection, onVariantSelect, analysisMode 
     setSelections(newSelections);
   };
 
+  const moveSelectionToTop = (index: number) => {
+    const newTopSelection = selections[index];
+    const newSelections = [newTopSelection];
+    for (let i = 0; i < selections.length; i++) {
+      if (index !== i) {
+        newSelections.push(selections[i]);
+      }
+    }
+    setSelections(newSelections);
+  };
+
   const submitSearch = () => {
     onVariantSelect(
       selections.map(s => s.selector),
@@ -123,8 +136,11 @@ export const VariantSearch = ({ currentSelection, onVariantSelect, analysisMode 
           <div>
             {selections.map((selection, index) => (
               <div className='flex' key={selection.id}>
-                <button className='mr-2 mb-6 outline-none' onClick={() => removeSelection(index)}>
-                  X
+                <button
+                  className='mb-6 outline-none hover:text-yellow-500'
+                  onClick={() => removeSelection(index + 1)}
+                >
+                  <AiFillDelete />
                 </button>
                 <div className='flex-grow'>
                   <VariantSearchField
@@ -155,25 +171,33 @@ export const VariantSearch = ({ currentSelection, onVariantSelect, analysisMode 
       <div className='flex flex-wrap'>
         <div className='flex-grow'>
           Baseline variant:
-          <div className='flex'>
-            <button className='mr-2 mb-6 outline-none invisible'>X</button>
-            <div className='flex-grow'>
-              <VariantSearchField
-                key={selections[0].id}
-                isSimple={false}
-                currentSelection={selections[0].selector}
-                onVariantSelect={newSelection => changeSelection(newSelection, 0)}
-                triggerSearch={submitSearch}
-              />
-            </div>
+          <div className='ml-3'>
+            <VariantSearchField
+              key={selections[0].id}
+              isSimple={false}
+              currentSelection={selections[0].selector}
+              onVariantSelect={newSelection => changeSelection(newSelection, 0)}
+              triggerSearch={submitSearch}
+            />
           </div>
           Comparing with:
           <div>
             {selections.slice(1).map((selection, index) => (
               <div className='flex' key={selection.id}>
-                <button className='mr-2 mb-6 outline-none' onClick={() => removeSelection(index + 1)}>
-                  X
-                </button>
+                <div className='mt-3 flex flex-column'>
+                  <button
+                    className='mb-2 outline-none hover:text-yellow-500'
+                    onClick={() => removeSelection(index + 1)}
+                  >
+                    <AiFillDelete />
+                  </button>
+                  <button
+                    className='outline-none hover:text-yellow-500'
+                    onClick={() => moveSelectionToTop(index + 1)}
+                  >
+                    <FaArrowUp />
+                  </button>
+                </div>
                 <div className='flex-grow'>
                   <VariantSearchField
                     key={selection.id}
