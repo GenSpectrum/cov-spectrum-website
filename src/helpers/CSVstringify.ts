25d611807a9e5e2d9d361b929a688c37be10787a
Anastasia Escher <escher@nexus.ethz.ch>
2022-08-15 17:02:54 +0200

    small fixes

--- src/helpers/CSVstringify.ts
+++ src/helpers/CSVstringify.ts
@@ -1,8 +1,7 @@
-const CSVstringify = async (data: any[]) => {
+const CSVstringify = (data: any[]) => {
   let result = '';
   if (data.length > 0) {
-    let header = Object.keys(data[0]);
-    result += header.join(',') + '\n';
+    result += Object.keys(data[0]).join(',') + '\n';
     data.slice(1).forEach(item => {
       result += Object.values(item).join(',') + '\n';
     });
