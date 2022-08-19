dd14a5098242f9031b75bbe8a9655abc7c31db3b
Anastasia Escher <escher@nexus.ethz.ch>
2022-08-19 15:15:05 +0200

    run prettier

--- src/models/wasteWater/WasteWaterLocationTimeChart.tsx
+++ src/models/wasteWater/WasteWaterLocationTimeChart.tsx
@@ -37,83 +37,78 @@ function deEscapeValueName(escapedName: string): string {
 
 const CHART_MARGIN_RIGHT = 15;
 
-export const WasteWaterLocationTimeChart = React.memo(
-  ({ variants }: Props): JSX.Element => {
-    const variantNames = variants.map(d => escapeValueName(d.name));
-    const dateMap: Map<UnifiedDay, { date: number; values: VariantMap; cis: CIMap }> = new Map();
+export const WasteWaterLocationTimeChart = React.memo(({ variants }: Props): JSX.Element => {
+  const variantNames = variants.map(d => escapeValueName(d.name));
+  const dateMap: Map<UnifiedDay, { date: number; values: VariantMap; cis: CIMap }> = new Map();
 
-    for (let { name, data } of variants) {
-      for (let { date, proportion, proportionCI } of data) {
-        if (!dateMap.has(date)) {
-          dateMap.set(date, {
-            date: date.dayjs.valueOf(),
-            values: {},
-            cis: {},
-          });
-        }
-        dateMap.get(date)!.values[escapeValueName(name)] = Math.max(proportion, 0);
-        dateMap.get(date)!.cis[escapeValueName(name)] = proportionCI;
+  for (let { name, data } of variants) {
+    for (let { date, proportion, proportionCI } of data) {
+      if (!dateMap.has(date)) {
+        dateMap.set(date, {
+          date: date.dayjs.valueOf(),
+          values: {},
+          cis: {},
+        });
       }
+      dateMap.get(date)!.values[escapeValueName(name)] = Math.max(proportion, 0);
+      dateMap.get(date)!.cis[escapeValueName(name)] = proportionCI;
     }
-
-    const plotData = [...dateMap.values()].sort((a, b) => a.date - b.date);
-    const today = Date.now();
-    const ticks = getTicks(plotData.map(({ date }) => ({ date: new Date(date) }))); // TODO This does not seem efficient
-    ticks.push(today);
-
-    return (
-      <Wrapper>
-        <TitleWrapper>Estimated prevalence in wastewater samples</TitleWrapper>
-        <ChartAndMetricsWrapper>
-          <ChartWrapper>
-            <ResponsiveContainer>
-              <ComposedChart
-                data={plotData}
-                margin={{ top: 6, right: CHART_MARGIN_RIGHT, left: 0, bottom: 0 }}
-              >
-                <XAxis
-                  dataKey='date'
-                  scale='time'
-                  type='number'
-                  tickFormatter={formatDate}
-                  domain={[(dataMin: any) => dataMin, () => today]}
-                  ticks={ticks}
-                />
-                <YAxis domain={['dataMin', 'auto']} />
-                <Tooltip
-                  formatter={(value: number, name: string, props: any) => {
-                    const escapedName = name.replace('values.', '');
-                    const [ciLower, ciUpper] = props.payload.cis[escapedName];
-                    return [
-                      (value * 100).toFixed(2) +
-                        '% [' +
-                        (ciLower * 100).toFixed(2) +
-                        '-' +
-                        (ciUpper * 100).toFixed(2) +
-                        '%]',
-                      deEscapeValueName(escapedName),
-                    ];
-                  }}
-                  labelFormatter={label => {
-                    return 'Date: ' + formatDate(label);
-                  }}
-                />
-                {variantNames.map(variant => (
-                  <Line
-                    type='monotone'
-                    dataKey={'values.' + variant}
-                    strokeWidth={3}
-                    stroke={wastewaterVariantColors[deEscapeValueName(variant)] ?? Utils.getRandomColorCode()}
-                    dot={false}
-                    isAnimationActive={false}
-                    key={variant}
-                  />
-                ))}
-              </ComposedChart>
-            </ResponsiveContainer>
-          </ChartWrapper>
-        </ChartAndMetricsWrapper>
-      </Wrapper>
-    );
   }
-);
+
+  const plotData = [...dateMap.values()].sort((a, b) => a.date - b.date);
+  const today = Date.now();
+  const ticks = getTicks(plotData.map(({ date }) => ({ date: new Date(date) }))); // TODO This does not seem efficient
+  ticks.push(today);
+
+  return (
+    <Wrapper>
+      <TitleWrapper>Estimated prevalence in wastewater samples</TitleWrapper>
+      <ChartAndMetricsWrapper>
+        <ChartWrapper>
+          <ResponsiveContainer>
+            <ComposedChart data={plotData} margin={{ top: 6, right: CHART_MARGIN_RIGHT, left: 0, bottom: 0 }}>
+              <XAxis
+                dataKey='date'
+                scale='time'
+                type='number'
+                tickFormatter={formatDate}
+                domain={[(dataMin: any) => dataMin, () => today]}
+                ticks={ticks}
+              />
+              <YAxis domain={['dataMin', 'auto']} />
+              <Tooltip
+                formatter={(value: number, name: string, props: any) => {
+                  const escapedName = name.replace('values.', '');
+                  const [ciLower, ciUpper] = props.payload.cis[escapedName];
+                  return [
+                    (value * 100).toFixed(2) +
+                      '% [' +
+                      (ciLower * 100).toFixed(2) +
+                      '-' +
+                      (ciUpper * 100).toFixed(2) +
+                      '%]',
+                    deEscapeValueName(escapedName),
+                  ];
+                }}
+                labelFormatter={label => {
+                  return 'Date: ' + formatDate(label);
+                }}
+              />
+              {variantNames.map(variant => (
+                <Line
+                  type='monotone'
+                  dataKey={'values.' + variant}
+                  strokeWidth={3}
+                  stroke={wastewaterVariantColors[deEscapeValueName(variant)] ?? Utils.getRandomColorCode()}
+                  dot={false}
+                  isAnimationActive={false}
+                  key={variant}
+                />
+              ))}
+            </ComposedChart>
+          </ResponsiveContainer>
+        </ChartWrapper>
+      </ChartAndMetricsWrapper>
+    </Wrapper>
+  );
+});
