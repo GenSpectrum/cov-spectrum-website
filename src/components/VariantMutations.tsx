6a68227490e7cb3c4767a9632c3f4ed4aa529fcd
Chaoran Chen <mail@chaoran-chen.de>
2022-09-29 17:07:17 +0200

    Improve data fetching for uniqueness calculation of mutations

--- src/components/VariantMutations.tsx
+++ src/components/VariantMutations.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MutationName } from './MutationName';
 import { decodeAAMutation, sortListByAAMutation } from '../helpers/aa-mutation';
@@ -7,9 +7,6 @@ import Loader from './Loader';
 import { useQuery } from '../helpers/query-hook';
 import { MutationProportionEntry } from '../data/MutationProportionEntry';
 import { fetchSamplesCount } from '../data/api-lapis';
-import { SequenceType } from '../data/SequenceType';
-import { VariantSelector } from '../data/VariantSelector';
-import { PromiseQueue } from '../helpers/PromiseQueue';
 import { ReferenceGenomeService } from '../services/ReferenceGenomeService';
 import NumericInput from 'react-numeric-input';
 import { LapisSelector } from '../data/LapisSelector';
@@ -53,13 +50,13 @@ const sortOptionLabels = {
 
 type SortOptions = typeof sortOptions[number];
 
-type MutationUniquenessMap = {
-  [key: string]: number | undefined;
+type MergedAAAndNucEntry = {
+  aa: MutationProportionEntryWithUniqueness;
+  nucs: MutationProportionEntryWithUniqueness[];
 };
 
-type MergedAAAndNucEntry = {
-  aa: MutationProportionEntry;
-  nucs: MutationProportionEntry[];
+type MutationProportionEntryWithUniqueness = MutationProportionEntry & {
+  uniqueness: number;
 };
 
 export const VariantMutations = ({ selector }: Props) => {
@@ -67,8 +64,6 @@ export const VariantMutations = ({ selector }: Props) => {
   const [showMergedList, setShowMergedList] = useState(false);
   const [commonAAMutationsSort, setCommonAAMutationsSort] = useState<SortOptions>('position');
   const [commonNucMutationsSort, setCommonNucMutationsSort] = useState<SortOptions>('position');
-  const [aaMutationUniqueness, setAAMutationUniqueness] = useState<MutationUniquenessMap>({});
-  const [nucMutationUniqueness, setNucMutationUniqueness] = useState<MutationUniquenessMap>({});
   const [minProportion, setMinProportion] = useState<number>(0.05);
   const [maxProportion, setMaxProportion] = useState<number>(1);
 
@@ -78,17 +73,63 @@ export const VariantMutations = ({ selector }: Props) => {
 
   const { width, ref } = useResizeDetector<HTMLDivElement>();
 
+  // Fetches all mutations and their counts for the country, date range, etc., but independent of the variant.
+  // The values will be used to calculate the Jaccard index
+  const overallMutationCounts = useQuery(
+    signal => {
+      const overallSelector: LapisSelector = {
+        location: selector.location,
+        dateRange: selector.dateRange,
+        host: selector.host,
+        samplingStrategy: selector.samplingStrategy,
+        qc: selector.qc,
+      };
+      return Promise.all([
+        MutationProportionData.fromApi(overallSelector, 'aa', signal, 0),
+        MutationProportionData.fromApi(overallSelector, 'nuc', signal, 0),
+      ]).then(results => {
+        const [aa, nuc] = results.map(d => {
+          const countMap = new Map();
+          for (const entry of d.payload) {
+            countMap.set(entry.mutation, entry.count);
+          }
+          return countMap;
+        });
+        return { aa, nuc };
+      });
+    },
+    [selector.location, selector.dateRange, selector.host, selector.samplingStrategy, selector.qc]
+  );
+
   const queryStatus = useQuery(
-    signal =>
-      Promise.all([
+    signal => {
+      if (!overallMutationCounts.data) {
+        return Promise.resolve(undefined);
+      }
+
+      return Promise.all([
         fetchSamplesCount(selector, signal),
         MutationProportionData.fromApi(selector, 'aa', signal),
         MutationProportionData.fromApi(selector, 'nuc', signal),
       ]).then(async ([variantCount, aaMutationDataset, nucMutationDataset]) => {
-        const aa = aaMutationDataset.payload;
-        const nuc = nucMutationDataset.payload;
+        const aa: MutationProportionEntryWithUniqueness[] = aaMutationDataset.payload.map(m => ({
+          ...m,
+          uniqueness: calculateJaccardSimilarity(
+            variantCount,
+            overallMutationCounts.data!.aa.get(m.mutation),
+            m.count
+          ),
+        }));
+        const nuc: MutationProportionEntryWithUniqueness[] = nucMutationDataset.payload.map(m => ({
+          ...m,
+          uniqueness: calculateJaccardSimilarity(
+            variantCount,
+            overallMutationCounts.data!.nuc.get(m.mutation),
+            m.count
+          ),
+        }));
         const aaMap = new Map<string, MergedAAAndNucEntry[]>();
-        const additionalNucs: MutationProportionEntry[] = [];
+        const additionalNucs: MutationProportionEntryWithUniqueness[] = [];
         for (let aaElement of aa) {
           const aaDecoded = decodeAAMutation(aaElement.mutation);
           const aaString = aaDecoded.gene + ':' + aaDecoded.position;
@@ -118,41 +159,10 @@ export const VariantMutations = ({ selector }: Props) => {
           mergedEntries,
           additionalNucs,
         };
-      }),
-    [selector]
+      });
+    },
+    [selector, overallMutationCounts]
   );
-
-  useEffect(() => {
-    if (!queryStatus.data) {
-      return;
-    }
-    const { aa, nuc, variantCount } = queryStatus.data;
-    // aa
-    // We use a PromiseQueue because fetching the count of all mutations can be very costly (for the server).
-    const aaFetchQueue = new PromiseQueue();
-    for (let aaElement of aa) {
-      aaFetchQueue.addTask(() =>
-        fetchUniquenessScore(aaElement, selector, variantCount, 'aa').then(uniqueness =>
-          setAAMutationUniqueness(prev => ({
-            ...prev,
-            [aaElement.mutation]: uniqueness,
-          }))
-        )
-      );
-    }
-    // nuc
-    const nucFetchQueue = new PromiseQueue();
-    for (let nucElement of nuc) {
-      nucFetchQueue.addTask(() =>
-        fetchUniquenessScore(nucElement, selector, variantCount, 'nuc').then(uniqueness => {
-          setNucMutationUniqueness(prev => ({
-            ...prev,
-            [nucElement.mutation]: uniqueness,
-          }));
-        })
-      );
-    }
-  }, [queryStatus.data, selector]);
 
   if (queryStatus.isLoading || !queryStatus.data) {
     return <Loader />;
@@ -230,7 +240,7 @@ export const VariantMutations = ({ selector }: Props) => {
           />
 
           <MutationList className='list-disc' width={width ? width : 1}>
-            {sortMergedEntries(data.mergedEntries, commonAAMutationsSort, aaMutationUniqueness)
+            {sortMergedEntries(data.mergedEntries, commonAAMutationsSort)
               .filter(({ aa }) => aa.proportion >= minProportion && aa.proportion <= maxProportion)
               .filter(({ aa }) => {
                 return !checked ? aa.mutation.slice(-1) !== '-' : true;
@@ -239,14 +249,14 @@ export const VariantMutations = ({ selector }: Props) => {
                 <MutationEntry key={aa.mutation}>
                   &#8226;
                   <MutationName mutation={aa.mutation} /> (<Proportion value={aa.proportion} />,{' '}
-                  <Uniqueness value={aaMutationUniqueness[aa.mutation]} />)
+                  <Uniqueness value={aa.uniqueness} />)
                   <ul className='list-circle'>
-                    {sortNucMutations(nucs, 'position', nucMutationUniqueness)
+                    {sortNucMutations(nucs, 'position')
                       .filter(nuc => nuc.proportion >= minProportion && nuc.proportion <= maxProportion)
                       .map(nuc => (
                         <MutationEntry key={nuc.mutation} style={{ display: 'inline-block' }}>
                           {nuc.mutation} (<Proportion value={nuc.proportion} />,{' '}
-                          <Uniqueness value={nucMutationUniqueness[nuc.mutation]} />)
+                          <Uniqueness value={nuc.uniqueness} />)
                         </MutationEntry>
                       ))}
                   </ul>
@@ -255,16 +265,15 @@ export const VariantMutations = ({ selector }: Props) => {
           </MutationList>
           <div className='ml-0 mt-4'>Additional nucleotide mutations:</div>
           <MutationList className='list-circle ml-6' width={width ? width : 1}>
-            {sortNucMutations(data.additionalNucs, commonAAMutationsSort, nucMutationUniqueness)
+            {sortNucMutations(data.additionalNucs, commonAAMutationsSort)
               .filter(({ proportion }) => proportion >= minProportion && proportion <= maxProportion)
               .filter(({ mutation }) => {
                 return !checked ? mutation.slice(-1) !== '-' : true;
               })
-              .map(({ mutation, proportion }) => {
+              .map(({ mutation, proportion, uniqueness }) => {
                 return (
                   <MutationEntry key={mutation} style={{ display: 'inline-block' }}>
-                    &#8226; {mutation} (<Proportion value={proportion} />,{' '}
-                    <Uniqueness value={nucMutationUniqueness[mutation]} />)
+                    &#8226; {mutation} (<Proportion value={proportion} />, <Uniqueness value={uniqueness} />)
                   </MutationEntry>
                 );
               })}
@@ -286,17 +295,17 @@ export const VariantMutations = ({ selector }: Props) => {
           />
 
           <MutationList className='list-disc' width={width ? width : 1}>
-            {sortAAMutations(data.aa, commonAAMutationsSort, aaMutationUniqueness)
+            {sortAAMutations(data.aa, commonAAMutationsSort)
               .filter(({ proportion }) => proportion >= minProportion && proportion <= maxProportion)
               .filter(({ mutation }) => {
                 return !checked ? mutation.slice(-1) !== '-' : true;
               })
-              .map(({ mutation, proportion }) => {
+              .map(({ mutation, proportion, uniqueness }) => {
                 return (
                   <MutationEntry key={mutation} style={{ display: 'inline-block' }}>
                     &#8226;
                     <MutationName mutation={mutation} /> (<Proportion value={proportion} />,{' '}
-                    <Uniqueness value={aaMutationUniqueness[mutation]} />)
+                    <Uniqueness value={uniqueness} />)
                   </MutationEntry>
                 );
               })}
@@ -316,16 +325,15 @@ export const VariantMutations = ({ selector }: Props) => {
             onSelect={setCommonNucMutationsSort}
           />
           <MutationList className='list-disc' width={width ? width : 1}>
-            {sortNucMutations(data.nuc, commonNucMutationsSort, nucMutationUniqueness)
+            {sortNucMutations(data.nuc, commonNucMutationsSort)
               .filter(({ proportion }) => proportion >= minProportion && proportion <= maxProportion)
               .filter(({ mutation }) => {
                 return !checked ? mutation.slice(-1) !== '-' : true;
               })
-              .map(({ mutation, proportion }) => {
+              .map(({ mutation, proportion, uniqueness }) => {
                 return (
                   <MutationEntry key={mutation} style={{ display: 'inline-block' }}>
-                    &#8226; {mutation} (<Proportion value={proportion} />,{' '}
-                    <Uniqueness value={nucMutationUniqueness[mutation]} />)
+                    &#8226; {mutation} (<Proportion value={proportion} />, <Uniqueness value={uniqueness} />)
                   </MutationEntry>
                 );
               })}
@@ -337,36 +345,23 @@ export const VariantMutations = ({ selector }: Props) => {
 };
 
 const sortAAMutations = (
-  entries: MutationProportionEntry[],
-  sortOption: SortOptions,
-  uniquenessMap: MutationUniquenessMap
-): MutationProportionEntry[] => {
+  entries: MutationProportionEntryWithUniqueness[],
+  sortOption: SortOptions
+): MutationProportionEntryWithUniqueness[] => {
   switch (sortOption) {
     case 'proportion':
       return [...entries].sort((a, b) => b.proportion - a.proportion);
     case 'position':
       return sortListByAAMutation(entries, x => x.mutation);
     case 'uniqueness':
-      return [...entries].sort((a, b) => {
-        if (uniquenessMap[a.mutation] === undefined && uniquenessMap[b.mutation] === undefined) {
-          return 0;
-        }
-        if (uniquenessMap[a.mutation] === undefined) {
-          return 1;
-        }
-        if (uniquenessMap[b.mutation] === undefined) {
-          return -1;
-        }
-        return uniquenessMap[b.mutation]! - uniquenessMap[a.mutation]!;
-      });
+      return [...entries].sort((a, b) => b.uniqueness - a.uniqueness);
   }
 };
 
 const sortNucMutations = (
-  entries: MutationProportionEntry[],
-  sortOption: SortOptions,
-  uniquenessMap: MutationUniquenessMap
-): MutationProportionEntry[] => {
+  entries: MutationProportionEntryWithUniqueness[],
+  sortOption: SortOptions
+): MutationProportionEntryWithUniqueness[] => {
   return [...entries].sort((a, b) => {
     switch (sortOption) {
       case 'proportion':
@@ -377,16 +372,7 @@ const sortNucMutations = (
           parseInt(b.mutation.substr(1, b.mutation.length - 2))
         );
       case 'uniqueness':
-        if (uniquenessMap[a.mutation] === undefined && uniquenessMap[b.mutation] === undefined) {
-          return 0;
-        }
-        if (uniquenessMap[a.mutation] === undefined) {
-          return 1;
-        }
-        if (uniquenessMap[b.mutation] === undefined) {
-          return -1;
-        }
-        return uniquenessMap[b.mutation]! - uniquenessMap[a.mutation]!;
+        return b.uniqueness - a.uniqueness;
       default:
         throw new Error('Unimplemented case');
     }
@@ -395,8 +381,7 @@ const sortNucMutations = (
 
 const sortMergedEntries = (
   entries: MergedAAAndNucEntry[],
-  sortOption: SortOptions,
-  uniquenessMap: MutationUniquenessMap
+  sortOption: SortOptions
 ): MergedAAAndNucEntry[] => {
   // TODO This is very redundant to sortAAMutations().
   switch (sortOption) {
@@ -405,18 +390,7 @@ const sortMergedEntries = (
     case 'position':
       return sortListByAAMutation(entries, x => x.aa.mutation);
     case 'uniqueness':
-      return [...entries].sort((a, b) => {
-        if (uniquenessMap[a.aa.mutation] === undefined && uniquenessMap[b.aa.mutation] === undefined) {
-          return 0;
-        }
-        if (uniquenessMap[a.aa.mutation] === undefined) {
-          return 1;
-        }
-        if (uniquenessMap[b.aa.mutation] === undefined) {
-          return -1;
-        }
-        return uniquenessMap[b.aa.mutation]! - uniquenessMap[a.aa.mutation]!;
-      });
+      return [...entries].sort((a, b) => b.aa.uniqueness - a.aa.uniqueness);
   }
 };
 
@@ -426,22 +400,6 @@ const Proportion = ({ value }: { value: number }) => (
 
 const Uniqueness = ({ value }: { value: number | undefined }) => {
   return <span className={sortOptionCssClass['uniqueness']}>{value?.toFixed(2) ?? '...'}</span>;
-};
-
-const fetchUniquenessScore = (
-  proportionEntry: MutationProportionEntry,
-  selector: LapisSelector,
-  variantCount: number,
-  type: SequenceType
-): Promise<number> => {
-  const mutSelector: VariantSelector =
-    type === 'aa'
-      ? { aaMutations: [proportionEntry.mutation] }
-      : { nucMutations: [proportionEntry.mutation] };
-  return fetchSamplesCount({
-    ...selector,
-    variant: mutSelector,
-  }).then(mutationCount => calculateJaccardSimilarity(variantCount, mutationCount, proportionEntry.count));
 };
 
 /**
