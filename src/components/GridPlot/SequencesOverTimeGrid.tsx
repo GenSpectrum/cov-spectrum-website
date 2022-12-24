2e7abbe3c4da07bde3e906ecf9ff205a706d9749
Chaoran Chen <mail@chaoran-chen.de>
2022-12-24 22:39:38 +0100

    Extract code to fetch and calculate pango lineage proportion data

--- src/components/GridPlot/SequencesOverTimeGrid.tsx
+++ src/components/GridPlot/SequencesOverTimeGrid.tsx
@@ -1,5 +1,5 @@
 import { LapisSelector } from '../../data/LapisSelector';
-import { QueryStatus, useQuery } from '../../helpers/query-hook';
+import { useQuery } from '../../helpers/query-hook';
 import { _fetchAggSamples, fetchDateCountSamples } from '../../data/api-lapis';
 import { PangoLineageAliasResolverService } from '../../services/PangoLineageAliasResolverService';
 import {
@@ -40,113 +40,14 @@ type Props = {
 
 export const SequencesOverTimeGrid = ({ selector, pangoLineage, portals, axisPortals, plotWidth }: Props) => {
   // Data fetching
-  const dataQuery: QueryStatus<[TmpEntry3[], TmpEntry5[]]> = useQuery(
-    signal =>
-      Promise.all([
-        (_fetchAggSamples(selector, ['date', 'nextcladePangoLineage'], signal) as Promise<TmpEntry[]>).then(
-          async data => {
-            const data2: TmpEntry2[] = [];
-            for (let d of data) {
-              data2.push({
-                ...d,
-                nextcladePangoLineageFullName: d.nextcladePangoLineage
-                  ? (await PangoLineageAliasResolverService.findFullName(d.nextcladePangoLineage)) ??
-                    d.nextcladePangoLineage
-                  : null,
-              });
-            }
-            return data2.filter(d => !!d.date && !!d.nextcladePangoLineage) as TmpEntry3[];
-          }
-        ),
-        fetchDateCountSamples(selector, signal).then(data => data.filter(d => !!d.date) as TmpEntry5[]),
-      ]),
-    [selector]
-  );
+  const dataQuery = useQuery(signal => fetchDatePangoLineageCount(selector, signal), [selector]);
 
   // Data transformation
   const data: GroupedData<TmpEntry6, string> | undefined = useMemo(() => {
     if (!dataQuery.data) {
       return undefined;
     }
-    const [datePangoLineageCount, dateCount] = dataQuery.data;
-    const currentLineage = pangoLineage;
-    const currentLineageFullName =
-      PangoLineageAliasResolverService.findFullNameUnsafeSync(currentLineage) ?? pangoLineage;
-    const dateRangeInData = globalDateCache.rangeFromDays(datePangoLineageCount.map(d => d.date));
-    const allDays = globalDateCache.daysFromRange(dateRangeInData);
-    const lineagesData = new SingleData(datePangoLineageCount)
-      .filter(
-        d =>
-          d.nextcladePangoLineage === currentLineage ||
-          d.nextcladePangoLineageFullName.startsWith(currentLineageFullName + '.')
-      )
-      .map(d => {
-        let lineage;
-        if (d.nextcladePangoLineage === pangoLineage) {
-          lineage = pangoLineage;
-        } else {
-          // These are the sub-lineages
-          const withoutPrefix = d.nextcladePangoLineageFullName.substring(currentLineageFullName.length + 1);
-          const firstSub =
-            withoutPrefix.indexOf('.') !== -1
-              ? withoutPrefix.substring(0, withoutPrefix.indexOf('.'))
-              : withoutPrefix;
-          lineage =
-            PangoLineageAliasResolverService.findAliasUnsafeSync(`${currentLineageFullName}.${firstSub}`) +
-            '*';
-        }
-        return {
-          date: d.date,
-          nextcladePangoLineage: lineage,
-          count: d.count,
-        };
-      })
-      .groupBy(e => e.nextcladePangoLineage)
-      .mapGroups((es, nextcladePangoLineage) => {
-        const dateData = es.groupBy(e => e.date);
-        const dateMap = new Map<UnifiedDay, number>();
-        dateData.data.forEach((ds, date) =>
-          dateMap.set(
-            date,
-            ds.data.reduce((prev, curr) => prev + curr.count, 0)
-          )
-        );
-        const reducedData: TmpEntry4[] = [];
-        dateMap.forEach((count, date) => reducedData.push({ nextcladePangoLineage, date, count }));
-        return new SingleData(reducedData);
-      })
-      .fill(
-        e => e.date,
-        allDays,
-        (date, nextcladePangoLineage) => ({
-          date,
-          nextcladePangoLineage,
-          count: 0,
-        })
-      )
-      .sort(sortDateAsc)
-      .rolling(7, rolling7SumCountCentered);
-    const wholeData = new SingleData(dateCount)
-      .fill(
-        e => e.date,
-        allDays,
-        date => ({
-          date,
-          count: 0,
-        })
-      )
-      .sort(sortDateAsc)
-      .rolling(7, rolling7SumCountCentered);
-    // TODO HACK(Chaoran) "as SingleData<TmpEntry4>" is wrong. Instead, the typing of divideBySingle should be improved.
-    const proportionData = lineagesData
-      .divideBySingle(
-        wholeData as SingleData<TmpEntry4>,
-        e => e.date,
-        e => e.count
-      )
-      .sortGroups(comparePangoLineages);
-
-    return proportionData;
+    return calculateProportionPerPangoLineage({ currentLineage: pangoLineage, ...dataQuery.data });
   }, [dataQuery, pangoLineage]);
 
   // View
@@ -172,4 +73,130 @@ export const SequencesOverTimeGrid = ({ selector, pangoLineage, portals, axisPor
       )}
     </>
   );
+};
+
+export const fetchDatePangoLineageCount = async (
+  selector: LapisSelector,
+  signal: AbortSignal
+): Promise<{ datePangoLineageCount: TmpEntry3[]; dateCount: TmpEntry5[] }> => {
+  const [datePangoLineageCount, dateCount] = await Promise.all([
+    (_fetchAggSamples(selector, ['date', 'nextcladePangoLineage'], signal) as Promise<TmpEntry[]>).then(
+      async data => {
+        const data2: TmpEntry2[] = [];
+        for (let d of data) {
+          data2.push({
+            ...d,
+            nextcladePangoLineageFullName: d.nextcladePangoLineage
+              ? (await PangoLineageAliasResolverService.findFullName(d.nextcladePangoLineage)) ??
+                d.nextcladePangoLineage
+              : null,
+          });
+        }
+        return data2.filter(d => !!d.date && !!d.nextcladePangoLineage) as TmpEntry3[];
+      }
+    ),
+    fetchDateCountSamples(selector, signal).then(data => data.filter(d => !!d.date) as TmpEntry5[]),
+  ]);
+  return { datePangoLineageCount, dateCount };
+};
+
+export const groupBySubLineage = ({
+  currentLineage,
+  datePangoLineageCount,
+}: {
+  currentLineage: string;
+  datePangoLineageCount: TmpEntry3[];
+}): GroupedData<TmpEntry4, string> => {
+  const currentLineageFullName =
+    PangoLineageAliasResolverService.findFullNameUnsafeSync(currentLineage) ?? currentLineage;
+  const dateRangeInData = globalDateCache.rangeFromDays(datePangoLineageCount.map(d => d.date));
+  const allDays = globalDateCache.daysFromRange(dateRangeInData);
+  return new SingleData(datePangoLineageCount)
+    .filter(
+      d =>
+        d.nextcladePangoLineage === currentLineage ||
+        d.nextcladePangoLineageFullName.startsWith(currentLineageFullName + '.')
+    )
+    .map(d => {
+      let lineage;
+      if (d.nextcladePangoLineage === currentLineage) {
+        lineage = currentLineage;
+      } else {
+        // These are the sub-lineages
+        const withoutPrefix = d.nextcladePangoLineageFullName.substring(currentLineageFullName.length + 1);
+        const firstSub =
+          withoutPrefix.indexOf('.') !== -1
+            ? withoutPrefix.substring(0, withoutPrefix.indexOf('.'))
+            : withoutPrefix;
+        lineage =
+          PangoLineageAliasResolverService.findAliasUnsafeSync(`${currentLineageFullName}.${firstSub}`) + '*';
+      }
+      return {
+        date: d.date,
+        nextcladePangoLineage: lineage,
+        count: d.count,
+      };
+    })
+    .groupBy(e => e.nextcladePangoLineage)
+    .mapGroups((es, nextcladePangoLineage) => {
+      const dateData = es.groupBy(e => e.date);
+      const dateMap = new Map<UnifiedDay, number>();
+      dateData.data.forEach((ds, date) =>
+        dateMap.set(
+          date,
+          ds.data.reduce((prev, curr) => prev + curr.count, 0)
+        )
+      );
+      const reducedData: TmpEntry4[] = [];
+      dateMap.forEach((count, date) => reducedData.push({ nextcladePangoLineage, date, count }));
+      return new SingleData(reducedData);
+    })
+    .fill(
+      e => e.date,
+      allDays,
+      (date, nextcladePangoLineage) => ({
+        date,
+        nextcladePangoLineage,
+        count: 0,
+      })
+    )
+    .sort(sortDateAsc);
+};
+
+export const calculateProportionPerPangoLineage = ({
+  currentLineage,
+  datePangoLineageCount,
+  dateCount,
+  performSmoothing = true,
+}: {
+  currentLineage: string;
+  datePangoLineageCount: TmpEntry3[];
+  dateCount: TmpEntry5[];
+  performSmoothing?: boolean;
+}): GroupedData<TmpEntry6, string> => {
+  const dateRangeInData = globalDateCache.rangeFromDays(datePangoLineageCount.map(d => d.date));
+  const allDays = globalDateCache.daysFromRange(dateRangeInData);
+  let lineagesData = groupBySubLineage({ currentLineage, datePangoLineageCount });
+  let wholeData = new SingleData(dateCount)
+    .fill(
+      e => e.date,
+      allDays,
+      date => ({
+        date,
+        count: 0,
+      })
+    )
+    .sort(sortDateAsc);
+  if (performSmoothing) {
+    lineagesData = lineagesData.rolling(7, rolling7SumCountCentered);
+    wholeData = wholeData.rolling(7, rolling7SumCountCentered);
+  }
+  // TODO HACK(Chaoran) "as SingleData<TmpEntry4>" is wrong. Instead, the typing of divideBySingle should be improved.
+  return lineagesData
+    .divideBySingle(
+      wholeData as SingleData<TmpEntry4>,
+      e => e.date,
+      e => e.count
+    )
+    .sortGroups(comparePangoLineages);
 };
