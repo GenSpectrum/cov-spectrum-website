6b462313809b519df95fe16b6d7132c66dfe7025
Jonas Kellerer <jonas.kellerer@tngtech.com>
2023-02-15 09:59:46 +0100

    feat: remove omicron story
    #722

--- src/stories/StoryRouter.tsx
+++ src/stories/StoryRouter.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router';
-import Omicron from './Omicron';
 
 const StoryRouter = () => {
   let { storyId } = useParams();
   switch (storyId!.toLowerCase()) {
-    case 'omicron':
-      return <Omicron />;
     default:
       return <Navigate to='/stories' />;
   }
