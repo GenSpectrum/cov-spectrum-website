fa6d764ef87f7de6883a7d5d99d95c97d92a842a
Chaoran Chen <mail@chaoran-chen.de>
2023-04-09 23:12:01 +0200

    fix: landing country was always Europe
    Closes #750

--- src/baseLocation.ts
+++ src/baseLocation.ts
@@ -1,23 +1,8 @@
 import { fetchLapisDataVersionDate } from './data/api-lapis';
-import { fetchCurrentUserCountry } from './data/api';
-import { LocationService } from './services/LocationService';
-
-export let baseLocation = 'Europe';
 
 (async () => {
   try {
     // Fetch current data version of LAPIS
     await fetchLapisDataVersionDate();
-
-    // Find out the country/region of the user
-    const [currentUserCountry, allLocationNames] = await Promise.all([
-      fetchCurrentUserCountry(),
-      LocationService.getAllLocationNames(),
-    ]);
-    if (currentUserCountry.country && allLocationNames.includes(currentUserCountry.country)) {
-      baseLocation = currentUserCountry.country;
-    } else if (currentUserCountry.region && allLocationNames.includes(currentUserCountry.region)) {
-      baseLocation = currentUserCountry.region;
-    }
   } catch (_) {}
 })();
