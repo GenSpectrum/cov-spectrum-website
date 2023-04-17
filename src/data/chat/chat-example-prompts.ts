f9bb35d842947089aab194b267008451ce82fad8
Chaoran Chen <mail@chaoran-chen.de>
2023-04-18 00:24:16 +0200

    feat(chat): add example prompts

--- src/data/chat/chat-example-prompts.ts
+++ src/data/chat/chat-example-prompts.ts
@@ -1,7 +1,7 @@
 export const chatExamplePrompts = [
   'In which country was BA.1 first found more than 10 times on a day?',
   'How many sequences where found in February 2022 in Germany that have the mutation 23048A?',
-  'Which 10 countries upload the most sequences in 2023?',
+  'Which 10 countries uploaded the most sequences in 2023?',
   "What's the name of the current US president?",
   'Which nucleotide mutations are present in more than half of all SARS-CoV-2 sequences?',
   'It has come to my attention that the ORF1a:L3606F mutation is quite intriguing, and I simply cannot resist the urge to discover which country in the vast expanse of South America has generated the highest number of sequences with this specific mutation.',
@@ -9,6 +9,13 @@ export const chatExamplePrompts = [
   'Which lineages have the mutations A23403G, A23063T, and C3037T?',
   'Which country submitted the most animal samples?',
   'For which animals do we have sequences?',
+  'How many sequences were submitted in the last 4 weeks?',
+  'Which are the most common 5 variants this year?',
+  'When and where was the first XBB.1.16 collected?',
+  'When was the first XBB.1.16 submitted?',
+  'Please tell me the most common 10 AA mutations that occur in lineage XBB',
+  'When did the S:484K first appear?',
+  'When did the S:484K first co-appear with S:501Y?',
 ];
 
 export function getRandomChatPrompt() {
