118f5f54bc2307462cf9fef34f312b6c91d06799
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-02-10 11:08:17 +0100

    fix: dont write hosts comma separated in url

--- src/data/HostSelector.ts
+++ src/data/HostSelector.ts
@@ -6,9 +6,9 @@ export type HostSelector = string[];
 export function addHostSelectorToUrlSearchParams(selector: HostSelector, params: URLSearchParams) {
   params.delete('host');
   if (selector.length > 0) {
-    // TODO We assume that the names of the hosts do not have a ",". Is it safe?
-    //  This is currently the case for all of our data.
-    params.set('host', selector.join(','));
+    selector.forEach(hostSelector => {
+      params.append('host', hostSelector);
+    });
   }
 }
 
