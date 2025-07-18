87767600345997554e66c6b38b9673c0c031b2b2
Chaoran Chen <mail@chaoran-chen.de>
2025-07-18 16:06:31 +0200

    Remove unused import

--- src/models/wasteWater/story/__tests__/WasteWaterStoryPage.test.tsx
+++ src/models/wasteWater/story/__tests__/WasteWaterStoryPage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { WasteWaterStoryPage } from '../WasteWaterStoryPage';
 import { MemoryRouter } from 'react-router-dom';
 import ResizeObserver from 'resize-observer-polyfill';
