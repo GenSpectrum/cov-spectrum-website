eaee69472314b1dc57b1173e57f69690e87eba30
Chaoran Chen <mail@chaoran-chen.de>
2025-11-12 17:55:52 +0100

    fix: sequence list download

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-type Props = {
+type ExternalLinkProps = {
   url: string;
   label?: string;
   newWindow?: boolean;
   children?: React.ReactNode;
 };
 
-export const ExternalLink = ({ url, children, newWindow = true, label }: Props) => {
+export const ExternalLink = ({ url, children, newWindow = true, label }: ExternalLinkProps) => {
   return (
     <a
       href={url}
@@ -18,5 +18,27 @@ export const ExternalLink = ({ url, children, newWindow = true, label }: Props) 
     >
       {children ? children : url}
     </a>
+  );
+};
+
+type ExternalLinkAsyncProps = {
+  urlFunc: () => Promise<string>;
+  label?: string;
+  newWindow?: boolean;
+  children?: React.ReactNode;
+};
+
+export const ExternalLinkAsync = ({ urlFunc, children, newWindow = true, label }: ExternalLinkAsyncProps) => {
+  return (
+    <button
+      title={label}
+      className='text-active-secondary'
+      onClick={async () => {
+        const url = await urlFunc();
+        window.open(url, newWindow ? '_blank' : '_self');
+      }}
+    >
+      {children ? children : label}
+    </button>
   );
 };
